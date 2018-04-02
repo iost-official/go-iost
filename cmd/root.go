51a3586de5f101909d97ab7a2d9ddb8810dc0011
bitree <bitree8@gmail.com>
2018-04-02 12:38:10 -0400

    cmd: call cmd (cobra) in main

--- cmd/root.go
+++ cmd/root.go
@@ -34,9 +34,11 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-// Uncomment the following line if your bare application
-// has an action associated with it:
-//	Run: func(cmd *cobra.Command, args []string) { },
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello world!")
+	},
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
@@ -68,8 +70,8 @@ func initConfig() {
 	}
 
 	viper.SetConfigName(".PrototypeWorks") // name of config file (without extension)
-	viper.AddConfigPath("$HOME")  // adding home directory as first search path
-	viper.AutomaticEnv()          // read in environment variables that match
+	viper.AddConfigPath("$HOME")           // adding home directory as first search path
+	viper.AutomaticEnv()                   // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
