60e5bb7f6ecd7ce66ad4f73970efb0866b36cd75
He Peijian <glisxx6@gmail.com>
2018-04-03 17:35:32 +0800

    add mock of iosbase

--- iosbase/blockchain.go
+++ iosbase/blockchain.go
@@ -1,5 +1,6 @@
 package iosbase
 
+//go:generate mockgen -destination mocks/mock_blockchain.go -package iosbase_mock -source blockchain.go -imports .=github.com/iost-official/PrototypeWorks/iosbase
 // Block chain
 type BlockChain interface {
 	Get(layer int) (Block, error)
