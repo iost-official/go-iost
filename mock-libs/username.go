de5773eec4df2643914eb19d392d462c3ab05a88
zrp <zrphercule@gmail.com>
2018-04-06 13:55:37 -0700

    username checker

---
+++ mock-libs/username.go
@@ -0,0 +1,19 @@
+package block
+
+import (
+	"iosbase/debug/debug"
+)
+
+func (this *NameType) set(s string) {
+	length := len(s)
+	debug.assert(length <= 15, "Name is too long")
+	for c := range s {
+		debug.assert(debug.assert(this.is_valid_char(c)), "Invalid char")
+	}
+	this.name = s
+}
+
+func (this *NameType) is_valid_char(c rune) {
+	str_map := tools.VALID_USERNAME
+	return str_map[c]
+}
