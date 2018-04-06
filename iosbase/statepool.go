495ce458238679a8f51307e47f5f0ea9573814c9
He Peijian <glisxx6@gmail.com>
2018-04-06 18:02:48 +0800

    complete implement of statepool and blockchain

--- iosbase/statepool.go
+++ iosbase/statepool.go
@@ -1,63 +1,100 @@
 package iosbase
 
-import "fmt"
+import (
+	"github.com/gomodule/redigo/redis"
+)
 
 //go:generate mockgen -destination mocks/mock_statepool.go -package iosbase_mock -source statepool.go -imports .=github.com/iost-official/PrototypeWorks/iosbase
 
+/*
+Current states of system
+*/
 type StatePool interface {
-	Add(utxo State) error
-	Find(StateHash []byte) (State, error)
-	GetSlice() ([]State, error)
+	Init() error
+	Close() error
+	Add(state State) error
+	Find(stateHash []byte) (State, error)
 	Del(StateHash []byte) error
+	Transact(block *Block) error
 }
 
 type StatePoolImpl struct {
-	stateMap map[string]State
+	cli redis.Conn
 }
 
+const (
+	Conn   = "tcp"
+	DBAddr = "localhost:6379"
+)
+
 func (sp *StatePoolImpl) Init() error {
+	var err error
+	sp.cli, err = redis.Dial(Conn, DBAddr)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (sp *StatePoolImpl) Close() error {
+	if sp.cli != nil {
+		sp.cli.Close()
+	}
 	return nil
 }
 
 func (sp *StatePoolImpl) Add(state State) error {
-	if sp.stateMap == nil {
-		sp.stateMap = make(map[string]State)
-	}
-	if s, err := sp.Find(state.Hash()); err == nil {
-		return fmt.Errorf("state_exist")
-		_ = s
-	} else {
-		sp.stateMap[string(state.Hash())] = state
-		return nil
+	_, err := sp.cli.Do("HMSET", state.Hash(),
+		"value", state.Value,
+		"script", state.Script,
+		"tx_hash", state.BirthTxHash)
+	if err != nil {
+		return err
 	}
 	return nil
 }
 
 func (sp *StatePoolImpl) Find(stateHash []byte) (State, error) {
-	if sp.stateMap == nil {
-		sp.stateMap = make(map[string]State)
+	var s State
+	reply, err := redis.Values(sp.cli.Do("HMGET", stateHash, "value", "script", "tx_hash"))
+	if err != nil {
+		return s, err
 	}
-	state, ok := sp.stateMap[string(stateHash)]
-	if ok {
-		return state, nil
-	} else {
-		return State{}, fmt.Errorf("not found")
+	_, err = redis.Scan(reply, &s.Value, &s.Script, s.BirthTxHash)
+	if err != nil {
+		return s, err
 	}
+	return s, nil
 }
 
 func (sp *StatePoolImpl) Del(stateHash []byte) error {
-	if sp.stateMap == nil {
-		sp.stateMap = make(map[string]State)
+	_, err := sp.cli.Do("DEL", stateHash)
+	if err != nil {
+		return err
 	}
-	_, ok := sp.stateMap[string(stateHash)]
-	if ok {
-		delete(sp.stateMap, string(stateHash))
-		return nil
-	} else {
-		return fmt.Errorf("state_not_exist")
-	}
+	return nil
 }
 
-func (sp *StatePoolImpl) Transact(t Tx) error {
+func (sp *StatePoolImpl) Transact(block *Block) error {
+	var txp TxPoolImpl
+	txp.Decode(block.Content)
+	txs, err := txp.GetSlice()
+	if err != nil {
+		return err
+	}
+	for _, tx := range txs {
+		for _, in := range tx.Inputs {
+			err = sp.Del(in.StateHash)
+			if err != nil {
+				return err
+			}
+		}
+		for _, out := range tx.Outputs {
+			err = sp.Add(out)
+			if err != nil {
+				return err
+			}
+		}
+	}
 	return nil
 }
