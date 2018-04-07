6b77b0a3d5b6d6e7867010c19c3c4e0df80f6b2a
bitree <bitree8@gmail.com>
2018-04-06 23:26:33 -0400

    main: add debug

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/iost-official/PrototypeWorks/cmd"
+	_ "github.com/iost-official/PrototypeWorks/debug"
 	_ "github.com/iost-official/PrototypeWorks/drp"
 	_ "github.com/iost-official/PrototypeWorks/eds"
 	_ "github.com/iost-official/PrototypeWorks/iosbase"
