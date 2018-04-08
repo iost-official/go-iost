566834237dff47f26d9213ceb50c04c1c8478c8b
bitree8 <bitree8@gmail.com>
2018-04-08 13:15:45 -0400

    *: gofmt

--- cmd/root.go
+++ cmd/root.go
@@ -70,8 +70,8 @@ func initConfig() {
 	}
 
 	viper.SetConfigName(".Go-IOS-Protocol") // name of config file (without extension)
-	viper.AddConfigPath("$HOME")           // adding home directory as first search path
-	viper.AutomaticEnv()                   // read in environment variables that match
+	viper.AddConfigPath("$HOME")            // adding home directory as first search path
+	viper.AutomaticEnv()                    // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
