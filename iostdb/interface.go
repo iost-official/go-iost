f5f2e0666aa8509a0be82de9255e9d23ede7058a
liangjn16 <ljnanest@gmail.com>
2018-04-12 18:50:03 +0800

    update database_interface

--- iostdb/interface.go
+++ iostdb/interface.go
@@ -2,7 +2,9 @@ package iostdb
 
 type Database interface {
 	Put(key []byte, value []byte) error
+	PutHM(key []byte, args ...[]byte) error
 	Get(key []byte) ([]byte, error)
+	GetHM(key []byte, args ...[]byte) ([][]byte, error)
 	Has(key []byte) (bool, error)
 	Delete(key []byte) error
 	Close()
