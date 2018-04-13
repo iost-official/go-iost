1cc798b93b71491a7b1acaf07c3cb4928839fb84
wagnzhihaiSF <1599315665@qq.com>
2018-04-13 11:33:19 +0800

    updates Client interface

--- drp/drpclient.go
+++ drp/drpclient.go
@@ -3,6 +3,6 @@ package drp
 type Client interface {
 	RequestRandom(seed []byte) []byte
 	AddServer(server Server)
-	OnReceiveProof()
-	OnReceiveSignOff()
+	OnReceivingProof()
+	OnReceivingSignOff()
 }
