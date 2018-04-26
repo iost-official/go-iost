218b85e7b0a8d8ce2d601a5c399aea0087356212
winedia <winedia@gmail.com>
2018-04-26 19:54:44 +0800

    增加dynamic property时间计算的单元测试

--- consensus/dpos/properties_test.go
+++ consensus/dpos/properties_test.go
@@ -40,3 +40,78 @@ func TestGlobalStaticProperty(t *testing.T) {
 		})
 	})
 }
+
+func TestGlobalDynamicProperty(t *testing.T) {
+	Convey("Test of global dynamic property", t, func() {
+		sp := NewGlobalStaticProperty(core.Member{"id1", []byte{}, []byte{}}, []string{"id0", "id1", "id2"})
+		dp := NewGlobalDynamicProperty()
+		dp.LastBlockNumber = 0
+		dp.TotalSlots = 0
+		dp.LastBlockTime = core.Timestamp{0}
+		startTs := core.Timestamp{70000}
+		bh := core.BlockHead{
+			Number: 1,
+			Time: startTs,
+			Witness: "id0",
+		}
+		dp.Update(&bh)
+
+		Convey("update first block", func() {
+			So(dp.LastBlockNumber,ShouldEqual, 1)
+			So(dp.TotalSlots, ShouldEqual, 1)
+		})
+
+		curSec := startTs.ToUnixSec() + 1
+		sec := TimeUntilNextSchedule(sp, dp, curSec)
+		Convey("in other's slot", func() {
+			curTs := core.GetTimestamp(curSec)
+			wit := WitnessOfTime(sp, dp, curTs)
+			So(wit, ShouldEqual, "id0")
+			So(sec, ShouldBeLessThanOrEqualTo, 3)
+		})
+
+		curSec += 2
+		timestamp := core.GetTimestamp(curSec)
+		Convey("in self's slot", func() {
+			wit := WitnessOfTime(sp, dp, timestamp)
+			So(wit, ShouldEqual, "id1")
+			wit = WitnessOfSec(sp, dp, curSec)
+			So(wit, ShouldEqual, "id1")
+		})
+
+		bh.Number = 2
+		bh.Time = timestamp
+		bh.Witness = "id1"
+		dp.Update(&bh)
+		Convey("update second block", func() {
+			So(dp.LastBlockNumber,ShouldEqual, 2)
+			So(dp.TotalSlots, ShouldEqual, 2)
+		})
+
+		curSec += 1
+		sec = TimeUntilNextSchedule(sp, dp, curSec)
+		Convey("in self's slot, but finished", func() {
+			So(sec, ShouldBeGreaterThanOrEqualTo, 6)
+			So(sec, ShouldBeLessThanOrEqualTo, 9)
+		})
+
+		curSec += 8
+		Convey("in self's slot and lost two previous blocks", func() {
+			curTs := core.GetTimestamp(curSec)
+			wit := WitnessOfTime(sp, dp, curTs)
+			So(wit, ShouldEqual, "id1")
+			wit = WitnessOfSec(sp, dp, curSec)
+			So(wit, ShouldEqual, "id1")
+		})
+
+		timestamp = core.GetTimestamp(curSec)
+		bh.Number = 3
+		bh.Time = timestamp
+		bh.Witness = "id1"
+		dp.Update(&bh)
+		Convey("update third block", func() {
+			So(dp.LastBlockNumber,ShouldEqual, 3)
+			So(dp.TotalSlots, ShouldEqual, 5)
+		})
+	})
+}
