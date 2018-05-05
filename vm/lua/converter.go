2ccd8118f9e2b00f1dcd80b6eaca8e6002c980ba
He Peijian <glisxx6@gmail.com>
2018-05-05 17:25:59 +0800

    add bool and nil type of value

--- vm/lua/converter.go
+++ vm/lua/converter.go
@@ -6,13 +6,21 @@ import (
 )
 
 func Lua2Core(value lua.LValue) state.Value {
-
+	var v state.Value
 	switch value.(type) {
+	case lua.LNumber:
+		vl := value.(*lua.LNumber)
+		v = state.MakeVFloat(float64(*vl))
 	case lua.LString:
-		v := state.MakeVString(value.String())
-		return &v
+		v = state.MakeVString(value.String())
+	case lua.LBool:
+		if value == lua.LTrue {
+			v = state.VTrue
+		} else {
+			v = state.VFalse
+		}
 	}
-	return nil
+	return v
 }
 
 func Core2Lua(value state.Value) lua.LValue {
