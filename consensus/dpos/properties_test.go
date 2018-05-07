583d5e43a40efb26b39773c3190f86dc4f0963c9
winedia <winedia@gmail.com>
2018-05-07 16:38:42 +0800

    为DPoS模块添加godoc，修改命名

--- consensus/dpos/properties_test.go
+++ consensus/dpos/properties_test.go
@@ -11,21 +11,21 @@ import (
 
 func TestGlobalStaticProperty(t *testing.T) {
 	Convey("Test of witness lists of static property", t, func() {
-		prop := NewGlobalStaticProperty(Account{"id0", []byte{}, []byte{}}, []string{"id1", "id2", "id3"})
+		prop := newGlobalStaticProperty(Account{"id0", []byte{}, []byte{}}, []string{"id1", "id2", "id3"})
 		Convey("New", func() {
 			So(prop.NumberOfWitnesses, ShouldEqual, 3)
 		})
 
-		prop.AddPendingWitness("id4")
-		prop.AddPendingWitness("id5")
+		prop.addPendingWitness("id4")
+		prop.addPendingWitness("id5")
 		Convey("Add pending witness", func() {
 			So(len(prop.PendingWitnessList), ShouldEqual, 2)
-			err := prop.AddPendingWitness("id4")
+			err := prop.addPendingWitness("id4")
 			So(err, ShouldNotBeNil)
 		})
 
 		Convey("Update lists", func() {
-			prop.UpdateWitnessLists([]string{"id3", "id5", "id1"})
+			prop.updateWitnessLists([]string{"id3", "id5", "id1"})
 			So(prop.WitnessList[0], ShouldEqual, "id1")
 			So(prop.WitnessList[1], ShouldEqual, "id3")
 			So(prop.WitnessList[2], ShouldEqual, "id5")
@@ -34,11 +34,11 @@ func TestGlobalStaticProperty(t *testing.T) {
 		})
 
 		Convey("Delete pending witness", func() {
-			err := prop.DeletePendingWitness("id4")
+			err := prop.deletePendingWitness("id4")
 			So(len(prop.PendingWitnessList), ShouldEqual, 1)
 			So(err, ShouldBeNil)
 
-			err = prop.DeletePendingWitness("id2")
+			err = prop.deletePendingWitness("id2")
 			So(err, ShouldNotBeNil)
 		})
 	})
@@ -46,8 +46,8 @@ func TestGlobalStaticProperty(t *testing.T) {
 
 func TestGlobalDynamicProperty(t *testing.T) {
 	Convey("Test of global dynamic property", t, func() {
-		sp := NewGlobalStaticProperty(account.Account{"id1", []byte{}, []byte{}}, []string{"id0", "id1", "id2"})
-		dp := NewGlobalDynamicProperty()
+		sp := newGlobalStaticProperty(account.Account{"id1", []byte{}, []byte{}}, []string{"id0", "id1", "id2"})
+		dp := newGlobalDynamicProperty()
 		dp.LastBlockNumber = 0
 		dp.TotalSlots = 0
 		dp.LastBlockTime = Timestamp{0}
@@ -57,7 +57,7 @@ func TestGlobalDynamicProperty(t *testing.T) {
 			Time:    startTs.Slot,
 			Witness: "id0",
 		}
-		dp.Update(&bh)
+		dp.update(&bh)
 
 		Convey("update first block", func() {
 			So(dp.LastBlockNumber, ShouldEqual, 1)
@@ -65,10 +65,10 @@ func TestGlobalDynamicProperty(t *testing.T) {
 		})
 
 		curSec := startTs.ToUnixSec() + 1
-		sec := TimeUntilNextSchedule(&sp, &dp, curSec)
+		sec := timeUntilNextSchedule(&sp, &dp, curSec)
 		Convey("in other's slot", func() {
 			curTs := GetTimestamp(curSec)
-			wit := WitnessOfTime(&sp, &dp, curTs)
+			wit := witnessOfTime(&sp, &dp, curTs)
 			So(wit, ShouldEqual, "id0")
 			So(sec, ShouldBeLessThanOrEqualTo, 3)
 		})
@@ -76,23 +76,23 @@ func TestGlobalDynamicProperty(t *testing.T) {
 		curSec += 2
 		timestamp := GetTimestamp(curSec)
 		Convey("in self's slot", func() {
-			wit := WitnessOfTime(&sp, &dp, timestamp)
+			wit := witnessOfTime(&sp, &dp, timestamp)
 			So(wit, ShouldEqual, "id1")
-			wit = WitnessOfSec(&sp, &dp, curSec)
+			wit = witnessOfSec(&sp, &dp, curSec)
 			So(wit, ShouldEqual, "id1")
 		})
 
 		bh.Number = 2
 		bh.Time = timestamp.Slot
 		bh.Witness = "id1"
-		dp.Update(&bh)
+		dp.update(&bh)
 		Convey("update second block", func() {
 			So(dp.LastBlockNumber, ShouldEqual, 2)
 			So(dp.TotalSlots, ShouldEqual, 2)
 		})
 
 		curSec += 1
-		sec = TimeUntilNextSchedule(&sp, &dp, curSec)
+		sec = timeUntilNextSchedule(&sp, &dp, curSec)
 		Convey("in self's slot, but finished", func() {
 			So(sec, ShouldBeGreaterThanOrEqualTo, 6)
 			So(sec, ShouldBeLessThanOrEqualTo, 9)
@@ -101,9 +101,9 @@ func TestGlobalDynamicProperty(t *testing.T) {
 		curSec += 8
 		Convey("in self's slot and lost two previous blocks", func() {
 			curTs := GetTimestamp(curSec)
-			wit := WitnessOfTime(&sp, &dp, curTs)
+			wit := witnessOfTime(&sp, &dp, curTs)
 			So(wit, ShouldEqual, "id1")
-			wit = WitnessOfSec(&sp, &dp, curSec)
+			wit = witnessOfSec(&sp, &dp, curSec)
 			So(wit, ShouldEqual, "id1")
 		})
 
@@ -111,7 +111,7 @@ func TestGlobalDynamicProperty(t *testing.T) {
 		bh.Number = 3
 		bh.Time = timestamp.Slot
 		bh.Witness = "id1"
-		dp.Update(&bh)
+		dp.update(&bh)
 		Convey("update third block", func() {
 			So(dp.LastBlockNumber, ShouldEqual, 3)
 			So(dp.TotalSlots, ShouldEqual, 5)
