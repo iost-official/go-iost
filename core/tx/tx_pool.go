82f56f7c3796b76940078ae7f99fa6b96531d2d4
ljnanest <ljnanest@gmail.com>
2018-05-10 14:39:38 +0800

    添加tx_pool

---
+++ core/tx/tx_pool.go
@@ -0,0 +1,50 @@
+package tx
+
+import (
+	"github.com/iost-official/prototype/common"
+	. "github.com/iost-official/prototype/core/tx"
+)
+
+type TxPool interface {
+	Add(tx Tx) error
+	Del(tx Tx) error
+	Get([]byte) ([]Tx, error)
+	GetSlice() ([]Tx, error)
+	Has(tx Tx) (bool, error)
+	Size() int32
+}
+
+type TxPoolImpl struct {
+	txMap map[string]Tx
+}
+
+func NewTxPoolImpl() *TxPoolImpl {
+	return &TxPoolImpl{txMap: make(map[string]Tx)}
+}
+
+func (tp *TxPoolImpl) Add(tx Tx) error {
+	tp.txMap[common.Base58Encode(tx.Hash())] = tx
+	return nil
+}
+
+func (tp *TxPoolImpl) Del(tx Tx) error {
+	delete(tp.txMap, common.Base58Encode(tx.Hash()))
+	return nil
+}
+
+func (tp *TxPoolImpl) GetSlice() ([]Tx, error) {
+	var txs []Tx = make([]Tx, 0)
+	for _, tx := range tp.txMap {
+		txs = append(txs, tx)
+	}
+	return txs, nil
+}
+
+func (tp *TxPoolImpl) Has(tx Tx) (bool, error) {
+	_, ok := tp.txMap[common.Base58Encode(tx.Hash())]
+	return ok, nil
+}
+
+func (tp *TxPoolImpl) Size() int {
+	return len(tp.txMap)
+}
---
+++ core/tx/txpool_test.go
@@ -0,0 +1,68 @@
+package tx
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/iost-official/prototype/vm/mocks"
+
+	. "github.com/iost-official/prototype/core/tx"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestTxPoolImpl(t *testing.T) {
+	Convey("Test of TxPool", t, func() {
+		txp := NewTxPoolImpl()
+		ctl := gomock.NewController(t)
+
+		mockContract := vm_mock.NewMockContract(ctl)
+		mockContract.EXPECT().Encode().AnyTimes().Return([]byte{1, 2, 3})
+		tx := NewTx(int64(0), mockContract)
+		Convey("Add", func() {
+			txp.Add(tx)
+			So(len(txp.txMap), ShouldEqual, 1)
+		})
+
+		Convey("Del", func() {
+			txp.Del(tx)
+			So(len(txp.txMap), ShouldEqual, 0)
+		})
+
+		/*
+			Convey("Find", func() {
+				txp.Add(tx)
+				tx2, err := txp.Find(tx.Hash())
+				So(err, ShouldBeNil)
+				So(tx2.Time, ShouldEqual, tx.Time)
+
+				_, err = txp.Find([]byte("hello"))
+				So(err, ShouldNotBeNil)
+			})
+		*/
+
+		Convey("Has", func() {
+			txp.Add(tx)
+			bt, err := txp.Has(tx)
+			So(err, ShouldBeNil)
+			So(bt, ShouldBeTrue)
+			tx2 := NewTx(int64(1), mockContract)
+			bt, _ = txp.Has(tx2)
+			So(bt, ShouldBeFalse)
+		})
+
+		Convey("GetSlice", func() {
+			txp.Add(tx)
+			s, err := txp.GetSlice()
+			So(err, ShouldBeNil)
+			So(len(s), ShouldEqual, 1)
+			So(s[0].Time, ShouldEqual, tx.Time)
+		})
+
+		Convey("Size", func() {
+			txp.Add(tx)
+			l := txp.Size()
+			So(l, ShouldEqual, 1)
+		})
+
+	})
+}
