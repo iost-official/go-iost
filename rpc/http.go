8992279087277cfe0f0b83863f4465b6edcd88d8
HowHsu <shadow1343023812@gmail.com>
2018-05-15 22:14:13 +0800

    finish a rough version of PublishTx() and GetBlock()

--- rpc/http.go
+++ rpc/http.go
@@ -2,51 +2,48 @@ package rpc
 
 import (
 	"context"
-	"error"
+	"encoding/json"
 	"fmt"
 
-	//	"github.com/iost-official/prototype/core/block"
+	"github.com/iost-official/prototype/core/block"
 	"github.com/iost-official/prototype/core/message"
 	"github.com/iost-official/prototype/core/tx"
 	"github.com/iost-official/prototype/network"
 )
 
+type BInfo struct {
+	Head  block.BlockHead
+	txCnt int
+}
 type HttpServer struct {
 }
 
 func (s *HttpServer) PublishTx(ctx context.Context, _tx *Transaction) (*Response, error) {
-	/*
-		var tx1 tx.Tx
-		if _tx==nil{
-			return &Response{Code: -1},fmt.Errorf("argument cannot be nil pointer")
-		}
-		err:=tx1.Decode(_tx.Tx)
-		if err!=nil{
-			return &Response{Code: -1},err
-		}
-		err = tx1.VerifySelf() //verify Publisher and Signers
-		if err != nil {
-			return &Response{Code: -1}, err
-		}
-		//broadcast the tx
-		router, _ := network.RouterFactory("base")
-		baseNet, _ := network.NewBaseNetwork(&(network.NetConifg{
-			ListenAddr: "127.0.0.1"}))
-		router.Init(baseNet, 12345)
 
-		net:= router.(*RouterImpl).base.(*BaseNetwork)
-		broadTx := message.Message{
-			Body:    tx1.Encode(),
-			ReqType: int32(network.ReqPublishTx),
-			From:    net.localNode.String(), //?
-		}
-		router.Broadcast(broadTx)
-		//add this tx to txpool
-		tp, _ := tx.TxPoolFactory("mem")
-		tp.Add(&tx1)
-		return &Response{Code: 0}, nil
-	*/
-	return nil, nil
+	var tx1 tx.Tx
+	if _tx == nil {
+		return &Response{Code: -1}, fmt.Errorf("argument cannot be nil pointer")
+	}
+	err := tx1.Decode(_tx.Tx)
+	if err != nil {
+		return &Response{Code: -1}, err
+	}
+	err = tx1.VerifySelf() //verify Publisher and Signers
+	if err != nil {
+		return &Response{Code: -1}, err
+	}
+	//broadcast the tx
+	router, _ := network.RouterFactory("base")
+
+	broadTx := message.Message{
+		Body:    tx1.Encode(),
+		ReqType: int32(network.ReqPublishTx),
+	}
+	router.Broadcast(broadTx)
+	//add this tx to txpool
+	tp, _ := tx.TxPoolFactory("mem") //TODO:in fact,we should find the txpool_mem,not create a new txpool_mem
+	tp.Add(&tx1)
+	return &Response{Code: 0}, nil
 }
 
 func (s *HttpServer) GetContract(ctx context.Context, tx *ContractKey) (*Contract, error) {
@@ -65,20 +62,27 @@ func (s *HttpServer) GetState(ctx context.Context, tx *Key) (*Value, error) {
 }
 
 func (s *HttpServer) GetBlock(ctx context.Context, bk *BlockKey) (*BlockInfo, error) {
-	/*
-		bc,err:=NewBlockChain()
-		if err!=nil{
-			return nil,err
-		}
-		layer:=bk.Layer
-		curLen=bc.Length()
-		if (layer>curLen-1) || (layer<0){
-			return nil,fmt.Errorf("out of bound")
-		}
-		bInfo:=bc.GetBlockByNumber(curLen-1-layer)
-		if bInfo==nil{
-			return nil,fmt.Errorf("cannot get BlockInfo")
-		}
-	*/
-	return nil, nil
+	bc, err := block.NewBlockChain() //we should get the instance of Chain,not to Create it again in the real version
+	if err != nil {
+		return nil, err
+	}
+	layer := bk.Layer //I think bk.Layer should be uint64,because bc.Length() is uint64
+	curLen := bc.Length()
+	if (layer < 0) || (uint64(layer) > curLen-1) {
+		return nil, fmt.Errorf("out of bound")
+	}
+	block := bc.GetBlockByNumber(curLen - 1 - uint64(layer))
+	if block == nil {
+		return nil, fmt.Errorf("cannot get BlockInfo")
+	}
+	//better to Encode BlockHead first?
+	binfo := BInfo{
+		Head:  block.Head,
+		txCnt: block.LenTx(),
+	}
+	b, err := json.Marshal(binfo)
+	if err != nil {
+		return nil, fmt.Errorf("json.Marshal failed: [%v]", err)
+	}
+	return &BlockInfo{Json: string(b)}, nil
 }
