d6958a131f43a08267506a6f6590373836be7368
He Peijian <glisxx6@gmail.com>
2018-05-15 13:48:15 +0800

    generate mock file of rpc

--- rpc/interface.go
+++ rpc/interface.go
@@ -1,3 +1,6 @@
 package rpc
 
 //go:generate protoc --go_out=plugins=grpc:. ./cli.proto
+
+//go:generate mockgen -destination mocks/mock_cli_client.go -package mock_rpc github.com/iost-official/prototype/rpc CliClient
+//go:generate mockgen -destination mocks/mock_cli_server.go -package mock_rpc github.com/iost-official/prototype/rpc CliServer
