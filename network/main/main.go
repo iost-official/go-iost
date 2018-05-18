222ca8cad6e06f910af4071cd1f96e1bcff11781
liming8519 <liming8519@gmail.com>
2018-05-18 13:15:42 +0800

    fix

--- network/main/main.go
+++ network/main/main.go
@@ -6,27 +6,48 @@ import (
 
 	"fmt"
 
+	"net"
+
 	"github.com/iost-official/prototype/core/message"
 	. "github.com/iost-official/prototype/network"
 	"github.com/iost-official/prototype/network/discover"
 )
 
-var listenPort = flag.String("p", "30302", "go run main.go or go run main.go -p 30305 -s 127.0.0.1:30302")
-var serverAddr = flag.String("s", "", "Specify local port 30302-30304, or other ports that have already started.")
-var id = flag.String("i", "", "server tag id")
+var serverAddr = flag.String("s", "", "server port 30304, or other ports that have already started.")
 var conf = initNetConf()
 
 func initNetConf() *NetConifg {
 	conf := &NetConifg{}
 	conf.SetLogPath("/tmp")
 	conf.SetNodeTablePath("/tmp")
-	conf.SetListenAddr("127.0.0.1")
+	conf.SetListenAddr("0.0.0.0")
 	return conf
 }
 
 func main() {
+	bootnodeStart()
 	//testBaseNetwork()
-	node, err := discover.ParseNode("84a8ecbeeb6d3f676da1b261c35c7cd15ae17f32b659a6f5ce7be2d60f6c16f9@127.0.0.1:30304")
+	//testBootNodeConn()
+}
+
+func testBootNodeConn() {
+	node, err := discover.ParseNode("84a8ecbeeb6d3f676da1b261c35c7cd15ae17f32b659a6f5ce7be2d60f6c16f9@18.219.254.124:30304")
+
+	if err != nil {
+		fmt.Errorf("parse boot node got err:%v", err)
+	}
+	conn, err := net.Dial("tcp", node.Addr())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	conn.Write([]byte("111"))
+	conn.Close()
+	fmt.Println("conn to remote success!")
+}
+
+func bootnodeStart() {
+	node, err := discover.ParseNode("84a8ecbeeb6d3f676da1b261c35c7cd15ae17f32b659a6f5ce7be2d60f6c16f9@0.0.0.0:30304")
 	if err != nil {
 		fmt.Errorf("parse boot node got err:%v", err)
 	}
@@ -44,9 +65,8 @@ func main() {
 		return
 	}
 	go router.Run()
-	fmt.Println("server starting")
+	fmt.Println("server starting", node.Addr())
 	select {}
-
 }
 
 func testBaseNetwork() {
@@ -59,7 +79,7 @@ func testBaseNetwork() {
 		rs = append(rs, router)
 	}
 	go func() {
-		req := message.Message{From: "sender", Time: time.Now().UnixNano(), To: "127.0.0.1:30303", Body: []byte{22, 11, 125}}
+		req := message.Message{From: "sender", Time: time.Now().UnixNano(), To: "0.0.0.0:30303", Body: []byte{22, 11, 125}}
 		for {
 			//rs[1].Send(req)
 			req.Body = append(req.Body, []byte{11}...)
