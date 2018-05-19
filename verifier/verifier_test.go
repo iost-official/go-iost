0098f9a2e4f9c00e101ebe12d0945683518311c7
hpj <hpj@aureliatech.com>
2018-05-20 02:07:11 +0800

    add genesis parser and fix bug of parsing string to value

--- verifier/verifier_test.go
+++ verifier/verifier_test.go
@@ -18,7 +18,41 @@ import (
 	"github.com/iost-official/prototype/vm"
 	"github.com/iost-official/prototype/vm/lua"
 	. "github.com/smartystreets/goconvey/convey"
+	"github.com/iost-official/prototype/vm/mocks"
 )
+
+func TestGenesisVerify(t *testing.T) {
+	Convey("Test of Genesis verify", t, func() {
+		Convey("Parse Contract", func() {
+			mockCtl := gomock.NewController(t)
+			pool := core_mock.NewMockPool(mockCtl)
+			var count int
+			var k, f, k2 state.Key
+			var v, v2 state.Value
+			pool.EXPECT().PutHM(gomock.Any(),gomock.Any(),gomock.Any()).Times(2).Do(func(key, field state.Key, value state.Value) error {
+				k ,f ,v = key, field, value
+				count ++
+				return nil
+			})
+			pool.EXPECT().Put(gomock.Any(), gomock.Any()).Do(func(key state.Key, value state.Value){
+				k2, v2 = key, value
+			})
+			pool.EXPECT().Copy().Return(pool)
+			contract := vm_mock.NewMockContract(mockCtl)
+			contract.EXPECT().Code().Return(`
+-- @PutHM iost abc f10000
+-- @PutHM iost def f1000
+-- @Put hello sworld
+`)
+			_, err := ParseGenesis(contract, pool)
+			So(err, ShouldBeNil)
+			So(count, ShouldEqual, 2)
+			So(k, ShouldEqual, state.Key("iost"))
+			So(v2.EncodeString(), ShouldEqual, "sworld")
+
+		})
+	})
+}
 
 func TestCacheVerifier(t *testing.T) {
 	Convey("Test of CacheVerifier", t, func() {
