c3215025180e2de56cbd8932762a51bbefd009a1
lilei <lileicool1@gmail.com>
2018-06-05 13:03:23 +0800

    添加recorder

---
+++ consensus/pob2/holder.go
@@ -0,0 +1,18 @@
+package pob2
+
+import (
+	"github.com/iost-official/prototype/account"
+	"github.com/iost-official/prototype/core/state"
+)
+
+type Holder struct {
+	self  account.Account
+	pool  state.Pool
+	spool ServiPool
+}
+
+type Watcher struct {
+	hp *Holder
+}
+
+var Data Holder
---
+++ consensus/pob2/servi.go
@@ -0,0 +1,50 @@
+package pob2
+
+import (
+	"math"
+
+	"github.com/iost-official/prototype/vm"
+)
+
+const base float64 = 1.0
+
+type Servi struct {
+	v     float64
+	owner vm.IOSTAccount
+}
+
+func (s *Servi) Incr(time int) {
+	s.v += base * float64(time)
+}
+
+func (s *Servi) Clear() {
+	s.v = s.v / 2
+	s.v = math.Floor(s.v)
+}
+
+type ServiPool struct {
+	hm map[string]*Servi
+}
+
+func (sp *ServiPool) User(iostAccount vm.IOSTAccount) *Servi {
+	if servi, ok := sp.hm[string(iostAccount)]; ok {
+		return servi
+	} else {
+		sp.hm[string(iostAccount)] = &Servi{owner: iostAccount}
+		return sp.hm[string(iostAccount)]
+	}
+}
+
+func (sp *ServiPool) BestUser() *Servi {
+	var bestk string
+	var best float64
+	for k, v := range sp.hm {
+		if v.v > best {
+			best = v.v
+			bestk = k
+		}
+	}
+	return sp.hm[bestk]
+}
+
+var StdServiPool = ServiPool{}
