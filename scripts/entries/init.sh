662837b992a558772a0704868f410b351fb919c3
He Peijian <glisxx6@gmail.com>
2018-06-06 19:39:23 +0800

    add function to set yml files

--- scripts/entries/init.sh
+++ scripts/entries/init.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-cp $1 $GOPATH/src/github.com/iost-official/prototype/imonitor/iserver.yml
+cp $GOPATH/src/github.com/iost-official/prototype/iserver/$1 $GOPATH/src/github.com/iost-official/prototype/imonitor/iserver.yml && echo "ok"
