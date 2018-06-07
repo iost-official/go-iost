883d24eae6ed5085a3fb6bd35f4a1b91f963afa2
HowHsu <shadow1343023812@gmail.com>
2018-06-07 21:03:18 +0800

    add Sendmoney() and GenAccount for sendtx.py

--- scripts/sendtx/sendtx.py
+++ scripts/sendtx/sendtx.py
@@ -21,15 +21,93 @@ def has(fn):
 	return os.path.exists(fn)
 
 def Buildwallet():
-	ret=wCommand("cd "+project_path+"iwallet;go build")
+ 	ret=wCommand("cd "+project_path+"iwallet;go build")
 	ret=wCommand("cp "+project_path+"iwallet/iwallet "+cur_path)
 	return True
+
+def GenAccounts():
+	if Buildwallet()==False:return False
+	accounts=[]
+	for i in range(0,100):
+		wCommand(cur_path+"iwallet account -c test")
+		f=open(HOME+"/.ssh/test_secp","r")
+		seckey=f.read()
+		f.close()
+		f=open(HOME+"/.ssh/test_secp.pub","r")
+		pubkey=f.read()
+		f.close()
+		accounts.extend([pubkey+'\n',seckey+'\n'])
+
+	fd=open(cur_path+"acc_list.txt","w")
+	fd.writelines(accounts)
+	fd.close()
+	return "SUCCESS"
+
+def Sendmoney():
+	fd=open(cur_path+"acc_list.txt","r")
+	lines=fd.readlines()
+	fd.close()
+	lto=[]
+	for i in range(0,len(lines),2):
+		lto.append(lines[i][:-1])
+	genesisPubkey="2BibFrAhc57FAd3sDJFbPqjwskBJb5zPDtecPWVRJ1jxT"
+	genesisSeckey="BRpwCKmVJiTTrPFi6igcSgvuzSiySd7Exxj7LGfqieW9"
+	constructAll(genesisPubkey,lto,0.1)
+	f1=open(HOME+"/.ssh/test_secp","w")
+	f1.write(genesisSeckey)
+	f1.close()
+	
+	f2=open(HOME+"/.ssh/test_secp.pub","w")
+	f2.write(genesisPubkey)
+	f2.close()
+
+	flist=[Compile,Sign,Publish,]
+	for func in flist:
+		if func()==False:
+			return "FAIL"
+	return "SUCCESS"
+
+
+
+def construct(_from,_to,money):
+	f=open(cur_path+"test/1to2.lua","w")
+	f.writelines([
+		'--- main 合约主入口\n',
+		'-- server1转账server2\n',
+		'-- @gas_limit 10000\n',
+		'-- @gas_price 0.001\n',
+		'-- @param_cnt 0\n',
+		'-- @return_cnt 0\n',
+		'function main()\n',
+		'	Transfer("'+_from+'","'+_to+'",'+str(money)+')\n',
+		'end\n',
+	])
+	f.close()
+
+def constructAll(genesisPubkey,lto,money):
+	f=open(cur_path+"test/1to2.lua","w")
+	lines=[
+		'--- main 合约主入口\n',
+		'-- server1转账server2\n',
+		'-- @gas_limit 10000\n',
+		'-- @gas_price 0.001\n',
+		'-- @param_cnt 0\n',
+		'-- @return_cnt 0\n',
+		'function main()\n',
+	]
+	for i in range(0,len(lto)):
+		lines.append('	Transfer("'+genesisPubkey+'","'+lto[i]+'",'+str(money)+')\n')
+	lines.append('end\n')
+	f.writelines(lines)
+	f.close()
+
+
 def Contract():
 	#open acc_list.txt
-	#pubkey
-	#seckey
+	#0.pubkey
+	#1.seckey
 	#...
-	fd=open(cur_path+"acc_list.txt")
+	fd=open(cur_path+"acc_list.txt","r")
 	lines=fd.readlines()
 	fd.close()
 	money=random.random()
@@ -40,19 +118,7 @@ def Contract():
 		id1=random.randint(0,len(lines)-1)
 		id1=id1-(id1&1)
 
-	f=open(cur_path+"test/1to2.lua","w")
-	f.writelines([
-		'--- main 合约主入口\n',
-		'-- server1转账server2\n',
-		'-- @gas_limit 10000\n',
-		'-- @gas_price 0.001\n',
-		'-- @param_cnt 0\n',
-		'-- @return_cnt 0\n',
-		'function main()\n',
-		'	Transfer("'+lines[id0][:-1]+'","'+lines[id1][:-1]+'",'+str(money)+')\n',
-		'end\n',
-	])
-	f.close()
+	construct(lines[id0][:-1],lines[id1][:-1],money)
 	#write pubkey and seckey to ~/.ssh/test_secp
 	f1=open(HOME+"/.ssh/test_secp","w")
 	f1.write(lines[id0+1][:-1])
@@ -95,10 +161,21 @@ def Publish():
 	return False
 
 if __name__ == "__main__":
-	ans="SUCCESS"
-	func_list=[Buildwallet,Contract,Compile,Sign,Publish,]
-	for func in func_list:
-		if func()==False:
-			ans="FAIL"
-			break
-	print(ans)
+	if(len(sys.argv)!=2):
+		sys.exit(1)
+
+	com=sys.argv[1]
+	if com=="genaccounts":
+		print(GenAccounts())
+		sys.exit(0)
+	if com=="sendtoall":
+		print(Sendmoney())
+		sys.exit(0)
+	if com=="sendtransaction":
+		ans="SUCCESS"
+		func_list=[Buildwallet,Contract,Compile,Sign,Publish,]
+		for func in func_list:
+			if func()==False:
+				ans="FAIL"
+				break
+		print(ans)
