1fe1a3e62a5d2073f269bc6a92e95e4d8da9cebf
He Peijian <glisxx6@gmail.com>
2018-06-08 22:01:17 +0800

    fix bug

--- scripts/entries/init.sh
+++ scripts/entries/init.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-rm $GOPATH/src/github.com/iost-official/prototype/imonitor/iserver.yml
+rm $GOPATH/src/github.com/iost-official/prototype/iserver/iserver.yml
 cp $GOPATH/src/github.com/iost-official/prototype/iserver/$1 $GOPATH/src/github.com/iost-official/prototype/imonitor/iserver.yml && echo "ok"
