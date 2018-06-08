3204f67a82edd51ca92b8e1e360ce07d14f9249e
Meng Laijun <mlj1991@gmail.com>
2018-06-08 16:58:02 +0800

    Arrange Makefile and shell script

--- lint.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-gometalinter --config $GOPATH/src/github.com/iost-official/prototype/gometalinter.json $GOPATH/src/github.com/iost-official/prototype/... |grep -v 'exported method\|function'
-echo "ok"
