97fa75c15a7ca3b179a2569779e44cf23aa039ff
He Peijian <glisxx6@gmail.com>
2018-06-08 21:57:48 +0800

    rm old iserver.yml

--- scripts/entries/init.sh
+++ scripts/entries/init.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
+rm $GOPATH/src/github.com/iost-official/prototype/imonitor/iserver.yml
 cp $GOPATH/src/github.com/iost-official/prototype/iserver/$1 $GOPATH/src/github.com/iost-official/prototype/imonitor/iserver.yml && echo "ok"
