abb7bd3fc6fcd04c5f681534bd86be9e08ceefe0
HowHsu <shadow1343023812@gmail.com>
2018-06-08 14:57:16 +0800

    add testWallet

---
+++ scripts/testWallet/test/1to2.lua
@@ -0,0 +1,9 @@
+--- main 合约主入口
+-- server1转账server2
+-- @gas_limit 10000
+-- @gas_price 0.001
+-- @param_cnt 0
+-- @return_cnt 0
+function main()
+	Transfer("2BibFrAhc57FAd3sDJFbPqjwskBJb5zPDtecPWVRJ1jxT","tUFikMypfNGxuJcNbfreh8LM893kAQVNTktVQRsFYuEU",1)
+end
