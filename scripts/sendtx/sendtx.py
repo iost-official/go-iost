071737653ec12302ae974d74beb11db9fc5dd14e
HowHsu <shadow1343023812@gmail.com>
2018-06-12 22:55:23 +0800

    modify sendtx.py

--- scripts/sendtx/sendtx.py
+++ scripts/sendtx/sendtx.py
@@ -118,6 +118,10 @@ def Contract():
 		id1=random.randint(0,len(lines)-1)
 		id1=id1-(id1&1)
 
+#for debug
+	id0=0
+	id1=2
+
 	construct(lines[id0][:-1],lines[id1][:-1],money)
 	_fd=open("./sendtx.log","w+")
 	_fd.write(lines[id0][:-1]+"  "+lines[id1][:-1]+"  "+str(money)+'   ')
@@ -178,7 +182,7 @@ if __name__ == "__main__":
 	if com=="sendtransaction":
 		ans="SUCCESS"
 		func_list=[Buildwallet,Contract,Compile,Sign,Publish,]
-		for i in range(0,1000):
+		for i in range(0,10):
 			for func in func_list:
 				if func()==False:
 					ans="FAIL"
