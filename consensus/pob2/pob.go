5d9f91ac5e36bc834a274a39ca9df53694257afb
HowHsu <shadow1343023812@gmail.com>
2018-06-16 23:44:26 +0800

    modify Txcnt

--- consensus/pob2/pob.go
+++ consensus/pob2/pob.go
@@ -78,7 +78,7 @@ type PoB struct {
 // NewPoB: 新建一个PoB实例
 // acc: 节点的Coinbase账户, bc: 基础链(从数据库读取), pool: 基础state池（从数据库读取）, witnessList: 见证节点列表
 func NewPoB(acc Account, bc block.Chain, pool state.Pool, witnessList []string /*, network core.Network*/) (*PoB, error) {
-	TxPerBlk = 100 + rand.Intn(900)
+	TxPerBlk = 2000
 	p := PoB{
 		account: acc,
 	}
