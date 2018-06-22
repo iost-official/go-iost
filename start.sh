e2fd39140c501b16015822ee5459f438811caf7e
Meng Laijun <mlj1991@gmail.com>
2018-06-22 21:22:31 +0800

    Update redis start command for Dockerfile

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nohup redis-server >> /var/lib/iserver/redis.log 2>&1 &
+nohup redis-server /etc/redis/redis.conf >> /var/lib/iserver/redis.log 2>&1 &
 #redis-server /etc/redis/redis.conf
 exec ./iserver --config /var/lib/iserver/iserver.yml $@
