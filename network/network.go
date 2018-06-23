a930df0bee6cbf4220516c00bd68ed3734a528bc
liuziran <1213092802@qq.com>
2018-06-24 00:14:13 +0800

    bug fix

--- network/network.go
+++ network/network.go
@@ -136,7 +136,7 @@ func (bn *BaseNetwork) Listen(port uint16) (<-chan message.Message, error) {
 				time.Sleep(2 * time.Second)
 				continue
 			}
-			// go bn.receiveLoop(conn)
+			go bn.receiveLoop(conn)
 		}
 	}()
 	//register
@@ -218,7 +218,7 @@ func (bn *BaseNetwork) dial(nodeStr string) (net.Conn, error) {
 			return conn, fmt.Errorf("dial tcp %v got err:%v", node.Addr(), err)
 		}
 		if conn != nil {
-			go bn.receiveLoop(conn)
+			// go bn.receiveLoop(conn)
 			peer := newPeer(conn, bn.localNode.Addr(), nodeStr)
 			log.Report(&log.MsgNode{SubType: log.Subtypes["MsgNode"][3], Log: node.Addr()})
 			log.Report(&log.MsgNode{SubType: log.Subtypes["MsgNode"][4], Log: strconv.Itoa(len(bn.peers.peers))})
