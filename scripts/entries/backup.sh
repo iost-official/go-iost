e53bf4b6a1ef3a5996fd017a1e190c87afa0eb0d
lilei <lileicool1@gmail.com>
2018-06-28 14:43:33 +0800

    删除无用目录

--- scripts/entries/backup.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-cd $GOPATH/src/github.com/iost-official/prototype/scripts/backup/
-python backup.py backup.config
--- scripts/entries/init.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-cp -f $GOPATH/src/github.com/iost-official/prototype/iserver/$1 /workdir/iserver.yml && echo "ok"
