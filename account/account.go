8c99bb1592b8b32116066e2ce1303a91316219bf
He Peijian <glisxx6@gmail.com>
2018-07-28 13:57:20 +0800

    change account.NewAccount to using crypto/rand

--- account/account.go
+++ account/account.go
@@ -1,12 +1,8 @@
 package account
 
 import (
-	"bytes"
-	"encoding/binary"
 	"fmt"
-	"math/rand"
-	"time"
-
+	"crypto/rand"
 	"github.com/iost-official/Go-IOS-Protocol/common"
 )
 
@@ -55,14 +51,8 @@ func (member *Account) GetId() string {
 }
 
 func randomSeckey() []byte {
-	rand.Seed(time.Now().UnixNano())
-	bin := new(bytes.Buffer)
-	for i := 0; i < 4; i++ {
-		b := make([]byte, 8)
-		binary.BigEndian.PutUint64(b, rand.Uint64())
-		bin.Write(b)
-	}
-	seckey := bin.Bytes()
+	seckey := make([]byte, 32)
+	rand.Read(seckey)
 	return seckey
 }
 
--- account/account_test.go
+++ account/account_test.go
@@ -5,8 +5,6 @@ import (
 
 	"bytes"
 
-	"fmt"
-
 	. "github.com/iost-official/Go-IOS-Protocol/common"
 	. "github.com/smartystreets/goconvey/convey"
 )
@@ -32,7 +30,7 @@ func TestMember(t *testing.T) {
 		Convey("sec to pub", func() {
 			m, err := NewAccount(Base58Decode("3BZ3HWs2nWucCCvLp7FRFv1K7RR3fAjjEQccf9EJrTv4"))
 			So(err, ShouldBeNil)
-			fmt.Println(Base58Encode(m.Pubkey))
+			So(Base58Encode(m.Pubkey), ShouldEqual, "iWgLQj3VTPN4dZnomuJMMCggv22LFw4nAkA6bmrVsmCo")
 		})
 	})
 }
