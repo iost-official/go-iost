704b6d97827525712a63b50348d5d5ef9b40828d
WangYu <wangyu0042@gmail.com>
2018-08-01 20:59:06 +0800

    [core/new_tx] add tx_receipt_test.go, bug fix

--- core/new_tx/tx_test.go
+++ core/new_tx/tx_test.go
@@ -21,6 +21,27 @@ func gentx() Tx {
 	return NewTx(int64(0), &lc, [1]byte)
 }
 */
+
+func TestAction(t *testing.T) {
+	Convey("Test of Action Data Structure", t, func() {
+		action := Action{
+			Contract:   "contract1",
+			ActionName: "actionname1",
+			Data:       "{\"num\": 1, \"message\": \"contract1\"}",
+		}
+
+		encode := action.Encode()
+
+		var action1 Action
+		err := action1.Decode(encode)
+		So(err, ShouldBeNil)
+
+		So(action.Contract == action1.Contract, ShouldBeTrue)
+		So(action.ActionName == action1.ActionName, ShouldBeTrue)
+		So(action.Data == action1.Data, ShouldBeTrue)
+	})
+}
+
 func TestTx(t *testing.T) {
 	Convey("Test of Tx Data Structure", t, func() {
 		actions := []Action{}
@@ -83,6 +104,29 @@ func TestTx(t *testing.T) {
 			So(err, ShouldEqual, nil)
 			hash1 = tx1.Hash()
 			So(bytes.Equal(hash, hash1), ShouldEqual, true)
+
+			So(tx.Id == tx1.Id, ShouldBeTrue)
+			So(tx.Time == tx1.Time, ShouldBeTrue)
+			So(len(tx.Actions) == len(tx1.Actions), ShouldBeTrue)
+			for i := 0; i < len(tx.Actions); i++ {
+				So(tx.Actions[i].Contract == tx1.Actions[i].Contract, ShouldBeTrue)
+				So(tx.Actions[i].ActionName == tx1.Actions[i].ActionName, ShouldBeTrue)
+				So(tx.Actions[i].Data == tx1.Actions[i].Data, ShouldBeTrue)
+			}
+			So(len(tx.Signers) == len(tx1.Signers), ShouldBeTrue)
+			for i := 0; i < len(tx.Signers); i++ {
+				So(bytes.Equal(tx.Signers[i], tx1.Signers[i]), ShouldBeTrue)
+			}
+			So(len(tx.Signs) == len(tx1.Signs), ShouldBeTrue)
+			for i := 0; i < len(tx.Signs); i++ {
+				So(tx.Signs[i].Algorithm == tx1.Signs[i].Algorithm, ShouldBeTrue)
+				So(bytes.Equal(tx.Signs[i].Pubkey, tx1.Signs[i].Pubkey), ShouldBeTrue)
+				So(bytes.Equal(tx.Signs[i].Sig, tx1.Signs[i].Sig), ShouldBeTrue)
+			}
+			So(tx.Publisher.Algorithm == tx1.Publisher.Algorithm, ShouldBeTrue)
+			So(bytes.Equal(tx.Publisher.Pubkey, tx1.Publisher.Pubkey), ShouldBeTrue)
+			So(bytes.Equal(tx.Publisher.Sig, tx1.Publisher.Sig), ShouldBeTrue)
+
 		})
 
 		Convey("sign and verify", func() {
