0940eef17eaf8342978c354a36e09a5b5bc83532
WangYu <wangyu0042@gmail.com>
2018-08-01 19:41:12 +0800

    [core/new_tx] add tx_test.go, verify signers in tx verifySelf

---
+++ core/new_tx/tx_test.go
@@ -0,0 +1,128 @@
+package tx
+
+import (
+	"testing"
+	//	"fmt"
+	"github.com/iost-official/Go-IOS-Protocol/account"
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/gogo/protobuf/proto"
+	"bytes"
+	"github.com/iost-official/Go-IOS-Protocol/common"
+)
+/*
+func gentx() Tx {
+	main := lua.NewMethod(0, "main", 0, 1)
+	code := `function main()
+				Put("hello", "world")
+				return "success"
+			end`
+	lc := lua.NewContract(vm.ContractInfo{Prefix: "test", GasLimit: 100, Price: 1, Publisher: vm.IOSTAccount("ahaha")}, code, main)
+
+	return NewTx(int64(0), &lc, [1]byte)
+}
+*/
+func TestTx(t *testing.T) {
+	Convey("Test of Tx Data Structure", t, func() {
+		actions := []Action{}
+		actions = append(actions, Action{
+			Contract:"contract1",
+			ActionName:"actionname1",
+			Data:"{\"num\": 1, \"message\": \"contract1\"}",
+		})
+		// seckey := common.Base58Decode("3BZ3HWs2nWucCCvLp7FRFv1K7RR3fAjjEQccf9EJrTv4")
+		// acc, err := account.NewAccount(seckey)
+		// So(err, ShouldEqual, nil)
+
+		a1, _ := account.NewAccount(nil)
+		a2, _ := account.NewAccount(nil)
+		a3, _ := account.NewAccount(nil)
+
+		Convey("proto marshal", func() {
+			tx := &TxRaw{
+				Id: "txproto",
+				Time: 1,
+				Actions:[]*ActionRaw{&ActionRaw{
+					Contract:"contract1",
+					ActionName:"actionname1",
+					Data:"{\"num\": 1, \"message\": \"contract1\"}",
+				}},
+				Signers:[][]byte{a1.Pubkey},
+			}
+			b, err := proto.Marshal(tx)
+			So(err, ShouldEqual, nil)
+
+			var tx1 *TxRaw = &TxRaw{}
+
+			err = proto.Unmarshal(b, tx1)
+			So(err, ShouldEqual, nil)
+
+			So("txproto", ShouldEqual, tx1.Id)
+		})
+
+		Convey("encode and decode", func() {
+			tx := NewTx(0, actions, [][]byte{a1.Pubkey})
+			tx1 := NewTx(1, []Action{}, [][]byte{})
+			hash := tx.Hash()
+
+			encode := tx.Encode()
+			err := tx1.Decode(encode)
+			So(err, ShouldEqual, nil)
+
+			hash1 := tx1.Hash()
+			So(bytes.Equal(hash, hash1), ShouldEqual, true)
+
+			sig, err := SignTxContent(tx, a1)
+			So(err, ShouldEqual, nil)
+
+			_, err = SignTx(tx, a1, sig)
+			So(err, ShouldEqual, nil)
+
+			hash = tx.Hash()
+			encode = tx.Encode()
+			err = tx1.Decode(encode)
+			So(err, ShouldEqual, nil)
+			hash1 = tx1.Hash()
+			So(bytes.Equal(hash, hash1), ShouldEqual, true)
+		})
+
+		Convey("sign and verify", func() {
+			tx := NewTx(0, actions, [][]byte{a1.Pubkey, a2.Pubkey})
+			sig1, err := SignTxContent(tx, a1)
+			So(tx.VerifySigner(sig1), ShouldBeTrue)
+			tx.Signs = append(tx.Signs, sig1)
+
+			err = tx.VerifySelf()
+			So(err.Error(), ShouldEqual, "signer not enough")
+
+			sig2, err := SignTxContent(tx, a2)
+			So(tx.VerifySigner(sig2), ShouldBeTrue)
+			tx.Signs = append(tx.Signs, sig2)
+
+			err = tx.VerifySelf()
+			So(err.Error(), ShouldEqual, "publisher error")
+
+			tx3, err := SignTx(tx, a3)
+			So(err, ShouldBeNil)
+			err = tx3.VerifySelf()
+			So(err, ShouldBeNil)
+
+			tx.Publisher = common.Signature{
+				Algorithm: common.Secp256k1,
+				Sig:       []byte("hello"),
+				Pubkey:    []byte("world"),
+			}
+			err = tx.VerifySelf()
+			So(err.Error(), ShouldEqual, "publisher error")
+
+			tx.Signs[0] = common.Signature{
+				Algorithm: common.Secp256k1,
+				Sig:       []byte("hello"),
+				Pubkey:    []byte("world"),
+			}
+			err = tx.VerifySelf()
+			So(err.Error(), ShouldEqual, "signer error")
+		})
+
+	})
+}
+
