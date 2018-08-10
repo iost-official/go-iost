6951cb949e24524d63f70fc672a3c949410f2d1d
Zhou Xiao <616702857@qq.com>
2018-08-10 12:05:26 +0800

    change block.go

--- core/new_block/block.go
+++ core/new_block/block.go
@@ -7,6 +7,7 @@ import (
 	"github.com/iost-official/Go-IOS-Protocol/common"
 	"github.com/iost-official/Go-IOS-Protocol/core/new_tx"
 	"github.com/gogo/protobuf/proto"
+	"github.com/iost-official/Go-IOS-Protocol/core/merkletree"
 )
 
 type Block struct {
@@ -16,66 +17,47 @@ type Block struct {
 	Receipts []tx.TxReceipt
 }
 
-/*
-func (d *Block) String() string {
-	str := "Block{\n"
-	str += "	BlockHead{\n"
-	str += "		Number: " + strconv.FormatInt(d.Head.Number, 10) + ",\n"
-	str += "		Time: " + strconv.FormatInt(d.Head.Time, 10) + ",\n"
-	str += "		Witness: " + d.Head.Witness + ",\n"
-	str += "	}\n"
-
-	str += "	Txs {\n"
-	//for _, tx := range d.Txs {
-	//	//str += tx.String()
-	//}
-	str += "	}\n"
-	str += "	Receipts {\n"
-	//for _, receipt := range d.Receipts {
-	//	str += receipt.String()
-	//}
-	str += "	}\n"
-	str += "}\n"
-	return str
-}
-*/
-
-func (d *Block) CalculateTxsHash() []byte {
+func (b *Block) CalculateTxsHash() []byte {
 	treeHash := make([]byte, 0)
-	for _, tx := range d.Txs {
+	for _, tx := range b.Txs {
 		treeHash = append(treeHash, tx.Publisher.Sig...)
 	}
 	return common.Sha256(treeHash)
 }
 
-func (d *Block) CalculateMerkleHash() []byte {
-	return nil
+func (b *Block) CalculateMerkleHash() ([]byte, error) {
+	m := merkletree.TXRMerkleTree{}
+	err := m.Build(b.Receipts)
+	if err != nil {
+		return nil, err
+	}
+	return m.RootHash()
 }
 
-func (d *Block) Encode() []byte {
+func (b *Block) Encode() []byte {
 	txs := make([][]byte, 0)
-	for _, t := range d.Txs {
+	for _, t := range b.Txs {
 		txs = append(txs, t.Encode())
 	}
 	rpts := make([][]byte, 0)
-	for _, r := range d.Receipts {
+	for _, r := range b.Receipts {
 		rpts = append(rpts, r.Encode())
 	}
 	br := &BlockRaw{
-		Head: &d.Head,
+		Head: &b.Head,
 		Txs: txs,
 		Receipts: rpts,
 	}
 
-	b, err := proto.Marshal(br)
+	brByte, err := proto.Marshal(br)
 	if err != nil {
 		panic(err)
 	}
-	d.hash = nil
-	return b
+	b.hash = brByte
+	return brByte
 }
 
-func (d *Block) Decode(bin []byte) (err error) {
+func (b *Block) Decode(bin []byte) (err error) {
 	br := &BlockRaw{}
 	defer func() {
 		if r := recover(); r != nil {
@@ -84,14 +66,14 @@ func (d *Block) Decode(bin []byte) (err error) {
 	}()
 
 	err = proto.Unmarshal(bin, br)
-	d.Head = *br.Head
+	b.Head = *br.Head
 	for _, t := range br.Txs {
 		var tt tx.Tx
 		err = tt.Decode(t)
 		if err != nil {
 			return err
 		}
-		d.Txs = append(d.Txs, tt)
+		b.Txs = append(b.Txs, tt)
 	}
 	for _, r := range br.Receipts {
 		var rcpt tx.TxReceipt
@@ -99,51 +81,51 @@ func (d *Block) Decode(bin []byte) (err error) {
 		if err != nil {
 			return err
 		}
-		d.Receipts = append(d.Receipts, rcpt)
+		b.Receipts = append(b.Receipts, rcpt)
 	}
 	return nil
 }
 
-func (d *Block) HashID() string {
-	id := d.Head.Witness +
-		strconv.FormatInt(d.Head.Time, 10) +
-		strconv.FormatInt(d.Head.Number, 10) +
-		strconv.FormatInt(d.Head.Version, 10)
+func (b *Block) HashID() string {
+	id := b.Head.Witness +
+		strconv.FormatInt(b.Head.Time, 10) +
+		strconv.FormatInt(b.Head.Number, 10) +
+		strconv.FormatInt(b.Head.Version, 10)
 	return id
 }
 
-func (d *Block) HeadHash() []byte {
-	if d.hash == nil {
-		d.hash = d.Head.Hash()
+func (b *Block) HeadHash() []byte {
+	if b.hash == nil {
+		b.hash = b.Head.Hash()
 	}
-	return d.hash
+	return b.hash
 }
 
-func (d *Block) GetTx(x int) tx.Tx {
-	if x < len(d.Txs) {
-		return d.Txs[x]
+func (b *Block) GetTx(x int) tx.Tx {
+	if x < len(b.Txs) {
+		return b.Txs[x]
 	} else {
 		return tx.Tx{}
 	}
 }
 
-func (d *Block) LenTx() int {
-	return len(d.Txs)
+func (b *Block) LenTx() int {
+	return len(b.Txs)
 }
 
-func (d *BlockHead) Encode() []byte {
-	bin, err := proto.Marshal(d)
+func (b *BlockHead) Encode() []byte {
+	bin, err := proto.Marshal(b)
 	if err != nil {
 		panic(err)
 	}
 	return bin
 }
 
-func (d *BlockHead) Decode(bin []byte) error {
-	err := proto.Unmarshal(bin, d)
+func (b *BlockHead) Decode(bin []byte) error {
+	err := proto.Unmarshal(bin, b)
 	return err
 }
 
-func (d *BlockHead) Hash() []byte {
-	return common.Sha256(d.Encode())
+func (b *BlockHead) Hash() []byte {
+	return common.Sha256(b.Encode())
 }
