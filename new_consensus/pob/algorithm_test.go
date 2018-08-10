9ba273bc055ec8c0e51b9d1bcd13bc449f94490d
winedia <winedia@gmail.com>
2018-08-10 18:28:53 +0800

    fix test bugs

--- new_consensus/pob/algorithm_test.go
+++ new_consensus/pob/algorithm_test.go
@@ -8,11 +8,11 @@ import (
 	"github.com/iost-official/Go-IOS-Protocol/account"
 	"github.com/iost-official/Go-IOS-Protocol/core/new_block"
 	"github.com/iost-official/Go-IOS-Protocol/common"
-	"github.com/iost-official/Go-IOS-Protocol/core/new_tx"
-	"github.com/iost-official/Go-IOS-Protocol/new_consensus/common"
 	"time"
+	"github.com/iost-official/Go-IOS-Protocol/core/new_tx"
 	"github.com/iost-official/Go-IOS-Protocol/core/new_txpool"
 	"github.com/iost-official/Go-IOS-Protocol/db"
+	"github.com/iost-official/Go-IOS-Protocol/new_consensus/common"
 )
 
 func TestConfirmNode(t *testing.T) {
@@ -20,49 +20,48 @@ func TestConfirmNode(t *testing.T) {
 
 		staticProp.WitnessList = []string{"id0", "id1", "id2", "id3", "id4"}
 		staticProp.NumberOfWitnesses = 5
-		bc, _ := blockcache.NewBlockCache(nil)
 		// Root of linked tree is confirmed
-		bc.LinkedTree = &blockcache.BlockCacheNode{
+		rootNode := &blockcache.BlockCacheNode{
 			Number:       1,
 			Witness:      "id0",
 			ConfirmUntil: 0,
 		}
 		Convey("Normal", func() {
-			node := addNode(bc.LinkedTree, 2, 0, "id1")
+			node := addNode(rootNode, 2, 0, "id1")
 			node = addNode(node, 3, 0, "id2")
 			node = addNode(node, 4, 0, "id3")
 			node = addNode(node, 5, 0, "id4")
 
-			confirmNode := calculateConfirm(node, bc.LinkedTree)
+			confirmNode := calculateConfirm(node, rootNode)
 			So(confirmNode.Number, ShouldEqual, 2)
 		})
 
 		Convey("Disordered normal", func() {
-			node := addNode(bc.LinkedTree, 2, 0, "id1")
+			node := addNode(rootNode, 2, 0, "id1")
 			node = addNode(node, 3, 0, "id2")
 			node = addNode(node, 4, 2, "id0")
 			node = addNode(node, 5, 4, "id2")
 			node = addNode(node, 6, 3, "id1")
 			node = addNode(node, 7, 0, "id3")
 
-			confirmNode := calculateConfirm(node, bc.LinkedTree)
+			confirmNode := calculateConfirm(node, rootNode)
 			So(confirmNode.Number, ShouldEqual, 4)
 		})
 
 		Convey("Disordered not enough", func() {
-			node := addNode(bc.LinkedTree, 2, 0, "id1")
+			node := addNode(rootNode, 2, 0, "id1")
 			node = addNode(node, 3, 0, "id2")
 			node = addNode(node, 4, 0, "id3")
 			node = addNode(node, 5, 3, "id4")
-			confirmNode := calculateConfirm(node, bc.LinkedTree)
+			confirmNode := calculateConfirm(node, rootNode)
 			So(confirmNode, ShouldBeNil)
 
 			node = addNode(node, 6, 4, "id5")
-			confirmNode = calculateConfirm(node, bc.LinkedTree)
+			confirmNode = calculateConfirm(node, rootNode)
 			So(confirmNode, ShouldBeNil)
 
 			node = addNode(node, 7, 2, "id0")
-			confirmNode = calculateConfirm(node, bc.LinkedTree)
+			confirmNode = calculateConfirm(node, rootNode)
 			So(confirmNode.Number, ShouldEqual, 4)
 		})
 	})
@@ -73,15 +72,14 @@ func TestPromoteWitness(t *testing.T) {
 	Convey("Test of Promote Witness", t, func() {
 		staticProp.WitnessList = []string{"id0", "id1", "id2"}
 		staticProp.NumberOfWitnesses = 3
-		bc, _ := blockcache.NewBlockCache(nil)
-		bc.LinkedTree = &blockcache.BlockCacheNode{
+		rootNode := &blockcache.BlockCacheNode{
 			Number:                1,
 			Witness:               "id0",
 			PendingWitnessList:    []string{"id0", "id1", "id2"},
 			LastWitnessListNumber: 1,
 		}
 		Convey("Normal", func() {
-			node := addNode(bc.LinkedTree, 2, 0, "id1")
+			node := addNode(rootNode, 2, 0, "id1")
 			node.PendingWitnessList = []string{"id3", "id2", "id1"}
 			node.LastWitnessListNumber = 2
 
@@ -95,14 +93,14 @@ func TestPromoteWitness(t *testing.T) {
 			node.PendingWitnessList = lastNode.PendingWitnessList
 			node.LastWitnessListNumber = lastNode.LastWitnessListNumber
 
-			confirmNode := calculateConfirm(node, bc.LinkedTree)
+			confirmNode := calculateConfirm(node, rootNode)
 			So(confirmNode.Number, ShouldEqual, 2)
 			promoteWitness(node, confirmNode)
 			So(staticProp.WitnessList[0], ShouldEqual, "id3")
 		})
 
 		Convey("Promote Newest", func() {
-			node := addNode(bc.LinkedTree, 2, 0, "id1")
+			node := addNode(rootNode, 2, 0, "id1")
 			node.PendingWitnessList = []string{"id3", "id2", "id1"}
 			node.LastWitnessListNumber = 2
 
@@ -121,7 +119,7 @@ func TestPromoteWitness(t *testing.T) {
 			node.PendingWitnessList = lastNode.PendingWitnessList
 			node.LastWitnessListNumber = lastNode.LastWitnessListNumber
 
-			confirmNode := calculateConfirm(node, bc.LinkedTree)
+			confirmNode := calculateConfirm(node, rootNode)
 			So(confirmNode, ShouldBeNil)
 
 			lastNode = node
@@ -129,7 +127,7 @@ func TestPromoteWitness(t *testing.T) {
 			node.PendingWitnessList = []string{"id5", "id2", "id3"}
 			node.LastWitnessListNumber = 6
 
-			confirmNode = calculateConfirm(node, bc.LinkedTree)
+			confirmNode = calculateConfirm(node, rootNode)
 			So(confirmNode.Number, ShouldEqual, 4)
 			promoteWitness(node, confirmNode)
 			So(staticProp.WitnessList[0], ShouldEqual, "id2")
@@ -139,17 +137,15 @@ func TestPromoteWitness(t *testing.T) {
 
 func TestNodeInfoUpdate(t *testing.T) {
 	Convey("Test of node info update", t, func() {
-
 		staticProp = newGlobalStaticProperty(account.Account{"id0",[]byte{}, []byte{}}, []string{"id0", "id1", "id2"})
-		bc, _ := blockcache.NewBlockCache(nil)
-		bc.LinkedTree = &blockcache.BlockCacheNode{
+		rootNode := &blockcache.BlockCacheNode{
 			Number:  1,
 			Witness: "id0",
 		}
 		staticProp.addSlotWitness(1,"id0")
 		staticProp.Watermark["id0"] = 2
 		Convey("Normal", func() {
-			node := addBlock(bc.LinkedTree, 2, "id1", 2)
+			node := addBlock(rootNode, 2, "id1", 2)
 			updateNodeInfo(node)
 			So(staticProp.Watermark["id1"], ShouldEqual, 3)
 			So(staticProp.hasSlotWitness(2,"id1"), ShouldBeTrue)
@@ -164,12 +160,12 @@ func TestNodeInfoUpdate(t *testing.T) {
 			So(staticProp.Watermark["id0"], ShouldEqual, 5)
 			So(staticProp.hasSlotWitness(4,"id0"), ShouldBeTrue)
 
-			node = calculateConfirm(node, bc.LinkedTree)
+			node = calculateConfirm(node, rootNode)
 			So(node.Number, ShouldEqual, 2)
 		})
 
 		Convey("Slot witness error", func() {
-			node := addBlock(bc.LinkedTree, 2, "id1", 2)
+			node := addBlock(rootNode, 2, "id1", 2)
 			updateNodeInfo(node)
 
 			node = addBlock(node, 3, "id1", 2)
@@ -178,7 +174,7 @@ func TestNodeInfoUpdate(t *testing.T) {
 		})
 
 		Convey("Watermark test", func() {
-			node := addBlock(bc.LinkedTree, 2, "id1", 2)
+			node := addBlock(rootNode, 2, "id1", 2)
 			updateNodeInfo(node)
 			So(node.ConfirmUntil, ShouldEqual, 0)
 			branchNode := node
@@ -189,7 +185,7 @@ func TestNodeInfoUpdate(t *testing.T) {
 			newNode := addBlock(branchNode, 3, "id0", 4)
 			updateNodeInfo(newNode)
 			So(newNode.ConfirmUntil, ShouldEqual, 2)
-			confirmNode := calculateConfirm(newNode, bc.LinkedTree)
+			confirmNode := calculateConfirm(newNode, rootNode)
 			So(confirmNode, ShouldBeNil)
 			So(staticProp.Watermark["id0"], ShouldEqual, 4)
 
@@ -200,12 +196,12 @@ func TestNodeInfoUpdate(t *testing.T) {
 			node = addBlock(node, 5, "id0", 7)
 			updateNodeInfo(node)
 			So(node.ConfirmUntil, ShouldEqual, 4)
-			confirmNode = calculateConfirm(node, bc.LinkedTree)
+			confirmNode = calculateConfirm(node, rootNode)
 			So(confirmNode, ShouldBeNil)
 
 			node = addBlock(node, 6, "id2", 9)
 			updateNodeInfo(node)
-			confirmNode = calculateConfirm(node, bc.LinkedTree)
+			confirmNode = calculateConfirm(node, rootNode)
 			So(confirmNode.Number, ShouldEqual, 4)
 		})
 	})
