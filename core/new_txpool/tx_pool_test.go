9cbc262ce37ef40c8add457db7b8a1b5114fcbf3
lilei <lileicool1@gmail.com>
2018-08-11 14:53:58 +0800

    fix import

--- core/new_txpool/tx_pool_test.go
+++ core/new_txpool/tx_pool_test.go
@@ -3,12 +3,12 @@ package new_txpool
 import (
 	"github.com/iost-official/Go-IOS-Protocol/account"
 	"github.com/iost-official/Go-IOS-Protocol/common"
-	"github.com/iost-official/Go-IOS-Protocol/consensus/common"
 	"github.com/iost-official/Go-IOS-Protocol/core/global"
 	"github.com/iost-official/Go-IOS-Protocol/core/new_block"
 	"github.com/iost-official/Go-IOS-Protocol/core/new_blockcache"
 	"github.com/iost-official/Go-IOS-Protocol/core/new_tx"
 	"github.com/iost-official/Go-IOS-Protocol/log"
+	"github.com/iost-official/Go-IOS-Protocol/new_consensus/common"
 	"github.com/iost-official/Go-IOS-Protocol/p2p"
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
