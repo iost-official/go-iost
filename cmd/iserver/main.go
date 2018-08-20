c52a92421fccbc860d8ebef1af7565eac2660a25
lilei <lileicool1@gmail.com>
2018-08-20 20:06:26 +0800

    add iserver.yml

--- cmd/iserver/main.go
+++ cmd/iserver/main.go
@@ -19,6 +19,7 @@ import (
 	"os/signal"
 	"syscall"
 
+	"fmt"
 	"github.com/iost-official/Go-IOS-Protocol/account"
 	"github.com/iost-official/Go-IOS-Protocol/common"
 	"github.com/iost-official/Go-IOS-Protocol/consensus"
@@ -28,6 +29,7 @@ import (
 	"github.com/iost-official/Go-IOS-Protocol/core/new_txpool"
 	"github.com/iost-official/Go-IOS-Protocol/log"
 	"github.com/iost-official/Go-IOS-Protocol/p2p"
+	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
 )
 
@@ -36,10 +38,19 @@ type ServerExit interface {
 	Stop()
 }
 
+var cfgFile string
+var logFile string
+var dbFile string
+var cpuprofile string
+var memprofile string
+
 var serverExit []ServerExit
 
 func main() {
 	//	cmd.Execute()
+
+	initConfig()
+
 	conf, err := common.NewConfig(viper.GetViper())
 	if err != nil {
 		os.Exit(1)
@@ -88,7 +99,7 @@ func main() {
 
 	serverExit = append(serverExit, p2pService)
 
-	accSecKey := viper.GetString("account.sec-key")
+	accSecKey := glb.Config().AccSecKey
 	//fmt.Printf("account.sec-key:  %v\n", accSecKey)
 	acc, err := account.NewAccount(common.Base58Decode(accSecKey))
 	if err != nil {
@@ -241,4 +252,33 @@ func exitLoop() {
 	signal.Stop(c)
 	close(exit)
 	os.Exit(0)
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".iserver" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".iserver")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	//fmt.Println(cfgFile)
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	} else {
+		panic(err)
+	}
+
 }
