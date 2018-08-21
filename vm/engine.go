a9b3f294057126d0570cc710bc3f648811cbcc33
He Peijian <glisxx6@gmail.com>
2018-08-21 14:53:17 +0800

    fix bug : return failed should get fee

--- vm/engine.go
+++ vm/engine.go
@@ -131,14 +131,14 @@ func (e *EngineImpl) Exec(tx0 *tx.Tx) (*tx.TxReceipt, error) {
 
 	for _, action := range tx0.Actions {
 
-		cost, status, receipts, err2 := e.runAction(action)
-		e.logger.Info("run action : %v, result is %v", action, status.Code)
-		e.logger.Debug("used cost > %v", cost)
-		e.logger.Debug("status > \n%v\n", status)
-		e.logger.Debug("receipts > \n%v\n", receipts)
+		cost, status, receipts, err := e.runAction(action)
+		e.logger.Debug("run action : %v", action)
+		e.logger.Debug("\tused cost > %v", cost)
+		e.logger.Debug("\tstatus > \n%v\n", status)
+		e.logger.Debug("\treceipts > \n%v\n", receipts)
 
-		if err2 != nil {
-			return nil, err2
+		if err != nil {
+			return nil, err
 		}
 
 		if cost == nil {
