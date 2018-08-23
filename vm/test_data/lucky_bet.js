8cca5efe38061af259fb8a291545b3902da84ffe
He Peijian <glisxx6@gmail.com>
2018-08-23 19:55:48 +0800

    use forEach

--- vm/test_data/lucky_bet.js
+++ vm/test_data/lucky_bet.js
@@ -23,24 +23,31 @@ class Contract {
         BlockChain.deposit(account, coins);
 
         if (this.tables[luckyNumber] === undefined) {
-            this.tables[luckyNumber] = []
+            this.tables[luckyNumber] = [];
         }
-
+        
         let isExist = false;
-        for (let i = 0; i < this.tables[luckyNumber].length; i ++) {
-            if (this.tables[luckyNumber][i].account === account){
-                this.tables[luckyNumber][i].coins += coins;
+        
+        this.tables[luckyNumber].forEach(function (record) {
+            if (record.account === account) {
+                record.coins += coins;
                 isExist = true;
             }
-        }
+        });
+
         if (!isExist) {
             this.tables[luckyNumber].push({ account:account, coins : coins})
         }
 
-        // if (this.tables[luckyNumber][account] === undefined) {
-        //     this.tables[luckyNumber][account] = coins
+        // if (this.tables[luckyNumber] === undefined) {
+        //     this.tables[luckyNumber] = {};
+        // }
+        //
+        // if (this.tables[luckyNumber].get(account) === undefined) {
+        //     this.tables[luckyNumber].set(account, coins);
         // } else {
-        //     this.tables[luckyNumber][account] += coins
+        //     let c = this.tables[luckyNumber].get(account);
+        //     this.tables[luckyNumber].set(account, c + coins);
         // }
 
         this.userNumber ++;
@@ -50,7 +57,7 @@ class Contract {
             let bi = JSON.parse(BlockChain.blockInfo());
             let bn = bi.number;
             let ph = bi.parent_hash;
-            if ( true /*this.lastLuckyBlock < 0 || bn - this.lastLuckyBlock >= 16 || bn > this.lastLuckyBlock && ph[ph.length-1] % 16 === 0*/) {
+            if ( this.lastLuckyBlock < 0 || bn - this.lastLuckyBlock >= 16 || bn > this.lastLuckyBlock && ph[ph.length-1] % 16 === 0) {
                 this.lastLuckyBlock = bn;
 
                 this.getReward(bn);
@@ -70,18 +77,16 @@ class Contract {
 
         _native_log("lucky number is "+ln);
 
-        let winTable = this.tables[ln];
-        _native_log("winTab is" + JSON.stringify(winTable));
 
-        // for (let [key, value] of winTable) {
-        //     totalVal += value
+        // for (let [key, value] of this.tables[ln]) {
+        //     totalVal += value;
         //     kNum ++
         // }
 
-        for (let i = 0; i < winTable.length; i ++) {
-            totalVal += winTable[i].coins;
-            kNum ++
-        }
+        this.tables[ln].forEach(function (record) {
+            totalVal += record.coins;
+            kNum ++;
+        });
 
         let result = {
             number: this.lastLuckyBlock,
@@ -95,13 +100,13 @@ class Contract {
             let unit = tc / totalVal;
             // for (let [key, value] of winTable) {
             //     BlockChain.withdraw(key, value * unit);
-            //     result.rewards.push({"key":key, "value": value * unit})
+            //     result.rewards.push({"account":key, "reward": value * unit})
             // }
-            for (let i = 0; i < winTable.length; i ++) {
-                let reward = winTable[i].coins * unit;
-                BlockChain.withdraw(winTable[i].account, reward);
-                result.rewards.push({"account":winTable[i].account, "reward": reward})
-            }
+            this.tables[ln].forEach(function (record) {
+                let reward = record.coins * unit;
+                BlockChain.withdraw(record.account, reward);
+                result.rewards.push({"account":record.account, "reward": reward})
+            });
         }
         let results =  this.results;
         results.push(result);
