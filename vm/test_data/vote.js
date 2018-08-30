520a720568de77045373bdbd9a33a114eaccf31b
WangYu <wangyu0042@gmail.com>
2018-08-30 21:32:13 +0800

    [vm] add test vote

--- vm/test_data/vote.js
+++ vm/test_data/vote.js
@@ -7,13 +7,18 @@ class VoteContract {
 		this.voteLockTime = 200;
 
 		this.currentProducerList = [];
-		this.currentProducerList[0] = "";
-		this.currentProducerList[this.producerNumber - 1] = "";
 		this.pendingProducerList = ["a", "b", "c", "d", "e", "f", "g"];
+		this.pendingBlockNumber = 0;
+		this.producerTable = {}
+		this.voteTable = {}
 
-		// todo init producer list pledge token
 		for (var i = 0; i < this.producerNumber; i++) {
-			this["producer-" + this.pendingProducerList[i]] = {
+			// todo init producer list pledge token
+			// var ret = BlockChain.deposit(this.pendingProducerList[i], this.producerRegisterFee);
+			// if (ret != 0) {
+			// 	throw new Error("deposit failed. ret = " + ret);
+			// }
+			this.producerTable[this.pendingProducerList[i]] = {
 				"loc": "",
 				"url": "",
 				"netId": "",
@@ -25,10 +30,10 @@ class VoteContract {
     }
 
 	_requireAuth(account) {
-		var ret = BlockChain.require_auth(account);
-		if (ret !== 0) {
-			throw new Error("require auth failed. ret = " + ret);
-		}
+		// var ret = BlockChain.requireAuth(account);
+		// if (ret !== 0) {
+		// 	throw new Error("require auth failed. ret = " + ret);
+		// }
 	}
 
 	_getBlockNumber() {
@@ -42,14 +47,14 @@ class VoteContract {
 	// register account as a producer, need to pledge token
     RegisterProducer(account, loc, url, netId) {
 		this._requireAuth(account);
-		if (this.hasOwnProperty("producer-" + account)) {
+		if (this.producerTable.hasOwnProperty(account)) {
 			throw new Error("producer exists");
 		}
 		var ret = BlockChain.deposit(account, this.producerRegisterFee);
 		if (ret != 0) {
 			throw new Error("deposit failed. ret = " + ret);
 		}
-		this["producer-" + account] = {
+		this.producerTable[account] = {
 			"loc": loc,
 			"url": url,
 			"netId": netId,
@@ -62,47 +67,53 @@ class VoteContract {
 	// update the information of a producer
     UpdateProducer(account, loc, url, netId) {
 		this._requireAuth(account);
-		if (!this.hasOwnProperty("producer-" + account)) {
+		if (!this.producerTable.hasOwnProperty(account)) {
 			throw new Error("producer not exists");
 		}
-		this["producer-" + account].loc = loc;
-		this["producer-" + account].url = url;
-		this["producer-" + account].netId = netId;
+		var pro = this.producerTable[account];
+		pro.loc = loc;
+		pro.url = url;
+		pro.netId = netId;
+		this.producerTable[account]	= pro;
     }
 
 	// producer log in as online state
     LogInProducer(account) {
 		this._requireAuth(account);
-		if (!this.hasOwnProperty("producer-" + account)) {
-			throw new Error("producer not exists");
+		if (this.producerTable.hasOwnProperty(account) === false) {
+			throw new Error("producer not exists, " + account);
 		}
-		this["producer-" + account].online = true;
+		var pro = this.producerTable[account];
+		pro.online = true;
+		this.producerTable[account] = pro;
     }
 
 	// producer log out as offline state
     LogOutProducer(account) {
 		this._requireAuth(account);
-		if (!this.hasOwnProperty("producer-" + account)) {
+		if (this.producerTable.hasOwnProperty(account) === false) {
 			throw new Error("producer not exists");
 		}
 		if (this.pendingProducerList.includes(account) || this.currentProducerList.includes(account)) {
 			throw new Error("producer in pending list or in current list, can't logout");
 		}
-		this["producer-" + account].online = false;
+		var pro = this.producerTable[account];
+		pro.online = false;
+		this.producerTable[account] = pro;
     }
 
 	// remove account from producer list
 	UnregisterProducer(account) {
 		this._requireAuth(account);
-		if (!this.hasOwnProperty("producer-" + account)) {
+		if (!this.producerTable.hasOwnProperty(account)) {
 			throw new Error("producer not exists");
 		}
 		if (this.pendingProducerList.includes(account) || this.currentProducerList.includes(account)) {
 			throw new Error("producer in pending list or in current list, can't unregist");
 		}
 		// will clear votes and score of the producer
-		delete this["producer-" + account];
-		delete this.preProducerMap(account);
+		this.producerTable[account] = undefined;
+		this.preProducerMap[account] = undefined;
 
 		var ret = BlockChain.withdraw(account, this.producerRegisterFee);
 		if (ret != 0) {
@@ -114,7 +125,7 @@ class VoteContract {
 	Vote(producer, voter, amount) {
 		this._requireAuth(voter);
 
-		if (!this.hasOwnProperty("producer-" + producer)) {
+		if (!this.producerTable.hasOwnProperty(producer)) {
 			throw new Error("producer not exists");
 		}
 
@@ -123,19 +134,22 @@ class VoteContract {
 			throw new Error("deposit failed. ret = " + ret);
 		}
 
-		// record the amount and time of the vote
-		if (this["vote-" + voter].hasOwnProperty(producer)) {
-			this["vote-" + voter][producer].amount += amount;
-		} else {
-			this["vote-" + voter][producer] = {};
-			this["vote-" + voter][producer].amount = amount;
+		if (!this.voteTable.hasOwnProperty(voter)) {
+			this.voteTable[voter] = {}
 		}
-		this["vote-" + voter][producer].time = _getBlockNumber();
+		// record the amount and time of the vote
+		if (this.voteTable[voter].hasOwnProperty(producer)) {
+			this.voteTable[voter][producer].amount += amount;
+		} else {
+			this.voteTable[voter][producer] = {};
+			this.voteTable[voter][producer].amount = amount;
+		}
+		this.voteTable[voter][producer].time = this._getBlockNumber();
 
 		// if producer's votes >= preProducerThreshold, then insert into preProducer map
-		this["producer-" + producer].votes += amount;
-		if (this["producer-" + producer].votes - amount < this.preProducerThreshold &&
-				this["producer-" + producer].votes >= this.preProducerThreshold) {
+		this.producerTable[producer].votes += amount;
+		if (this.producerTable[producer].votes - amount < this.preProducerThreshold &&
+				this.producerTable[producer].votes >= this.preProducerThreshold) {
 			this.preProducerMap[producer] = true
 		}
 	}
@@ -144,23 +158,25 @@ class VoteContract {
 	Unvote(producer, voter, amount) {
 		this._requireAuth(voter);
 
-		if (!this["vote-" + voter].hasOwnProperty(producer) || this["vote-" + voter][producer].amount < amount) {
+		if (!this.voteTable.hasOwnProperty(voter) || !this.voteTable[voter].hasOwnProperty(producer) || 
+				this.voteTable[voter][producer].amount < amount) {
 			throw new Error("producer not voted or vote amount less than expected")
 		}
-		if (this["vote-" + voter][producer].time + this.voteLockTime > _getBlockNumber()) {
+		if (this.voteTable[voter][producer].time + this.voteLockTime > this._getBlockNumber()) {
 			throw new Error("vote still lockd")
 		}
 
-		this["vote-" + voter][producer].amount -= amount;
+		this.voteTable[voter][producer].amount -= amount;
 
 		// if producer not exist, it's because producer has unregistered, do nothing
-		if (this.hasOwnProperty("producer-" + producer)) {
-			this["producer-" + producer].votes -= amount;
+		if (this.producerTable.hasOwnProperty(producer)) {
+			var ori = this.producerTable[producer].votes;
+			this.producerTable[producer].votes = Math.max(0, this.producerTable[producer].votes - amount);
 			// if producer's votes < preProducerThreshold, then delete from preProducer map
-			if (this["producer-" + producer].votes + amount >= this.preProducerThreshold && 
-					this["producer-" + producer].votes < this.preProducerThreshold) {
+			if (ori >= this.preProducerThreshold && 
+					this.producerTable[producer].votes < this.preProducerThreshold) {
 				// todo cut down score?
-				delete this.preProducerMap[producer];
+				this.preProducerMap[producer] = undefined;
 			}
 		}
 
@@ -178,9 +194,10 @@ class VoteContract {
 
 		// add scores for preProducerMap
 		var preList = [];	// list of producers whose vote > threshold
+		var that = this;
 		Object.keys(this.preProducerMap).forEach(function(key){
-			var pro = this["producer-" + key]
-			if (pro.votes >= this.preProducerThreshold && pro.online === true) {
+			var pro = that.producerTable[key]
+			if (!that.pendingProducerList.includes(key) && pro.votes >= that.preProducerThreshold && pro.online === true) {
 				preList.push({
 					"key": key,
 					"votes": pro.votes,
@@ -194,23 +211,24 @@ class VoteContract {
 			if (this.pendingProducerList.includes(key)) {
 				continue;
 			}
-			delta = preList[i].votes - this.preProducerThreshold;
-			this["producer-" + key].score += delta;
+			var delta = preList[i].votes - this.preProducerThreshold;
+			this.producerTable[key].score += delta;
 			preList[i].score += delta;
 		}
+		_native_log("pre list len = " + preList.length)
 
 		// sort according to score in reversed order
-		scoreCmp = function(a, b) {
+		var scoreCmp = function(a, b) {
 			return b.score - a.score;
 		}
 		preList.sort(scoreCmp);
 
 		// update pending list
 		var replaceNum = Math.min(preList.length, Math.floor(this.producerNumber / 6));
-		oldPreList = this.pendingProducerList.map(function(x){
+		var oldPreList = this.pendingProducerList.map(function(x){
 			return {
 				"key": x,
-				"score": this["producer-" + x].score
+				"score": that.producerTable[x].score
 			};
 		});
 		oldPreList.sort(scoreCmp);
@@ -220,14 +238,15 @@ class VoteContract {
 			oldPreList.push(preList[i]);
 		}
 		oldPreList.sort(scoreCmp);
-		newList = oldPreList.slice(0, this.producerNumber);
+		var newList = oldPreList.slice(0, this.producerNumber);
 
 		this.currentProducerList = this.pendingProducerList;
 		this.pendingProducerList = newList.map(x => x.key);
+		this.pendingBlockNumber = this._getBlockNumber();
 
 		for (var i = 0; i < this.producerNumber; i++) {
 			if (!this.pendingProducerList.includes(this.currentProducerList[i])) {
-				this["producer-" + this.currentProducerList[i]].score = 0;
+				this.producerTable[this.currentProducerList[i]].score = 0;
 			}
 		}
 	}
