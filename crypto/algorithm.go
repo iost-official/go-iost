d570e76733e938a6f4878f6bb9ab75b6b6b7bda4
Meng Laijun <mlj1991@gmail.com>
2018-09-15 22:08:08 +0800

    Add comment of crypto

--- crypto/algorithm.go
+++ crypto/algorithm.go
@@ -4,6 +4,7 @@ import (
 	"github.com/iost-official/Go-IOS-Protocol/crypto/backend"
 )
 
+// AlgorithmBackend is the interface of algorithm backend
 type AlgorithmBackend interface {
 	Sign(message []byte, seckey []byte) []byte
 	Verify(message []byte, pubkey []byte, sig []byte) bool
@@ -11,8 +12,10 @@ type AlgorithmBackend interface {
 	GenSeckey() []byte
 }
 
+// Algorithm is the crypto algorithm of signature
 type Algorithm uint8
 
+// Algorithm list
 const (
 	_ Algorithm = iota
 	Secp256k1
@@ -30,18 +33,22 @@ func (a Algorithm) getBackend() AlgorithmBackend {
 	}
 }
 
+// Sign will signature the message with seckey
 func (a Algorithm) Sign(message []byte, seckey []byte) []byte {
 	return a.getBackend().Sign(message, seckey)
 }
 
+// Verify will verify the message with pubkey and sig
 func (a Algorithm) Verify(message []byte, pubkey []byte, sig []byte) bool {
 	return a.getBackend().Verify(message, pubkey, sig)
 }
 
+// GetPubkey will get the public key of the secret key
 func (a Algorithm) GetPubkey(seckey []byte) []byte {
 	return a.getBackend().GetPubkey(seckey)
 }
 
+// GenSeckey will generate the secret key
 func (a Algorithm) GenSeckey() []byte {
 	return a.getBackend().GenSeckey()
 }
