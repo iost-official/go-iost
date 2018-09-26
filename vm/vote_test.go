826cf3c383b667e12e620992e92dab88ace73218
WangYu <wangyu0042@gmail.com>
2018-09-26 18:10:26 +0800

    [vm] UpdateCode contract use B64Encode

--- vm/vote_test.go
+++ vm/vote_test.go
@@ -462,7 +462,7 @@ func TestJS_Vote(t *testing.T) {
 			}
 
 			So(js.vi.Servi(testID[0]), ShouldEqual, 91054999)
-			So(js.vi.Balance(testID[0]), ShouldEqual, 3900000000880240)
+			So(js.vi.Balance(testID[0]), ShouldEqual, 3900000000880238)
 			So(js.vi.Balance(host.ContractAccountPrefix+"iost.bonus"), ShouldEqual, 20092)
 			act2 = tx.NewAction("iost.bonus", "ClaimBonus", fmt.Sprintf(`["%v", %d]`, testID[0], 91054999))
 
@@ -477,7 +477,7 @@ func TestJS_Vote(t *testing.T) {
 
 			So(js.vi.Servi(testID[0]), ShouldEqual, 0)
 			So(js.vi.Balance(host.ContractAccountPrefix+"iost.bonus"), ShouldEqual, 116)
-			So(js.vi.Balance(testID[0]), ShouldEqual, 3900000000899509)
+			So(js.vi.Balance(testID[0]), ShouldEqual, 3900000000899507)
 		})
 
 		Convey("test of vote update", func() {
