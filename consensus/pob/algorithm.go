28ab9cd641b565b3f5943b46107e07e24b2ed5ae
Zhou Xiao <616702857@qq.com>
2018-10-20 19:01:44 +0800

    Update consensus/pob/algorithm.go

--- consensus/pob/algorithm.go
+++ consensus/pob/algorithm.go
@@ -32,7 +32,7 @@ var (
 
 func generateBlock(account *account.Account, txPool txpool.TxPool, db db.MVCCDB) (*block.Block, error) {
 	ilog.Info("[pob] generate Block start")
-	limitTime := time.NewTimer(time.Second / 20)
+	limitTime := time.NewTimer(time.Millisecond * 800)
 	txIter, head := txPool.TxIterator()
 	topBlock := head.Block
 	blk := block.Block{
