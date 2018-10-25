0fc558ca78d12331ce45d313065f109cb18d0727
Zhou Xiao <616702857@qq.com>
2018-10-25 13:36:14 +0800

    Update consensus/pob/algorithm.go

--- consensus/pob/algorithm.go
+++ consensus/pob/algorithm.go
@@ -183,12 +183,7 @@ func updateWaterMark(node *blockcache.BlockCacheNode) {
 }
 
 func updateLib(node *blockcache.BlockCacheNode, bc blockcache.BlockCache) {
-	ilog.Infof("[pob] start to updateLib")
 	confirmedNode := calculateConfirm(node, bc.LinkedRoot())
-	if confirmedNode == nil {
-		ilog.Infof("[pob] start to draw")
-		ilog.Infof("[pob]" + bc.Draw())
-	}
 	if confirmedNode != nil {
 		ilog.Infof("[pob] flush start, number: %d, hash = %v", node.Number, common.Base58Encode(node.Block.HeadHash()))
 		bc.Flush(confirmedNode)
