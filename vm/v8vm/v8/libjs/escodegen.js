27dff4079b84fe2bca299ebb469911f1169c6954
WangYu <yu@iost.io>
2018-11-16 17:56:00 +0800

    [vm/v8vm/v8] modify webpack escodegen.js libraryTarget to use commonjs2

--- vm/v8vm/v8/libjs/escodegen.js
+++ vm/v8vm/v8/libjs/escodegen.js
@@ -1,14 +1,5 @@
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory();
-	else if(typeof define === 'function' && define.amd)
-		define([], factory);
-	else if(typeof exports === 'object')
-		exports["escodegen"] = factory();
-	else
-		root["escodegen"] = factory();
-})(window, function() {
-return /******/ (function(modules) { // webpackBootstrap
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -317,4 +308,3 @@ eval("module.exports = {\"_from\":\"escodegen@^1.11.0\",\"_id\":\"escodegen@1.11
 /***/ })
 
 /******/ });
-});
