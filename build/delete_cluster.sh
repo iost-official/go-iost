b0abce0517e0ad4b111af55cbc54165f7e600951
Meng Laijun <mlj1991@gmail.com>
2018-11-19 14:25:37 +0800

    Update e2e test for travis ci

--- build/delete_cluster.sh
+++ build/delete_cluster.sh
@@ -10,7 +10,9 @@ cd build/k8s/
 
 echo "Delete test cluster $NAME in k8s"
 kubectl delete -f iserver.yaml -n $NAME --ignore-not-found
+kubectl delete -f itest.yaml -n $NAME --ignore-not-found
 kubectl delete pvc -l k8s-app=iserver -n $NAME
 kubectl delete configmap iserver-config -n $NAME --ignore-not-found
+kubectl delete configmap itest-config -n $NAME --ignore-not-found
 kubectl delete namespace $NAME --ignore-not-found
 
