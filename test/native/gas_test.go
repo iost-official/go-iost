c3d372479ea2c1bf193ee138599e8ee090c284ae
WangYu <wangyu0042@gmail.com>
2018-11-22 19:59:36 +0800

    fix gas test

--- test/native/gas_test.go
+++ test/native/gas_test.go
@@ -80,7 +80,7 @@ func gasTestInit() (*native.Impl, *host.Host, *contract.Contract, string, db.MVC
 	if err != nil {
 		panic(err)
 	}
-	h.DB().Put("auth.iost@"+testAcc+"-auth", database.MustMarshal(string(acc1)))
+	h.DB().MPut("auth.iost"+"-auth", testAcc, database.MustMarshal(string(acc1)))
 
 	otherAcc := "user2"
 	user2 := getAccount(otherAcc, "5oyBNyBeMFUKndGF8E3xkxmS3qugdYbwntSu8NEYtvC2DMmVcXgtmBqRxCLUCjxcu9zdcH3RkfKec3Q2xeiG48RL")
@@ -88,7 +88,7 @@ func gasTestInit() (*native.Impl, *host.Host, *contract.Contract, string, db.MVC
 	if err != nil {
 		panic(err)
 	}
-	h.DB().Put("auth.iost@"+otherAcc+"-auth", database.MustMarshal(string(acc2)))
+	h.DB().MPut("auth.iost"+"-auth", otherAcc, database.MustMarshal(string(acc2)))
 
 	h.Context().Set("number", int64(1))
 	h.Context().Set("time", int64(1541576370*1e9))
