e8e6932a8cd08a0e96c00f0fe006b82ff967ebee
WangYu <wangyu0042@gmail.com>
2018-11-23 21:06:54 +0800

    [vm/monitor] require stack_height less than 6

--- test/integration/test_data/nest0.js
+++ test/integration/test_data/nest0.js
@@ -7,6 +7,22 @@ class Contract {
         storage.put("nest0", "000")
         BlockChain.call(conName, abiName, args)
     }
+
+    sh0(conName) {
+        BlockChain.call(conName, "sh1", JSON.stringify([BlockChain.contractName()]));
+    }
+
+    sh2(conName) {
+        BlockChain.call(conName, "sh3", JSON.stringify([BlockChain.contractName()]));
+    }
+
+    sh4(conName) {
+        BlockChain.call(conName, "sh5", JSON.stringify([BlockChain.contractName()]));
+    }
+
+    sh6(conName) {
+        BlockChain.call(conName, "sh7", JSON.stringify([BlockChain.contractName()]));
+    }
 }
 
 module.exports = Contract;
