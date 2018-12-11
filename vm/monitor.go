16e9dce17dc0201407667bba1c24e3b453a0f9a6
liushiqi9 <liushiqi9@gmail.com>
2018-12-11 23:16:49 +0800

    change monitor pool size to 400

--- vm/monitor.go
+++ vm/monitor.go
@@ -223,7 +223,7 @@ func Factory(lang string) VM {
 		vm.Init()
 		return &vm
 	case "javascript":
-		vm := v8.NewVMPool(10, 2000)
+		vm := v8.NewVMPool(10, 400)
 		vm.Init()
 		//vm.SetJSPath(jsPath)
 		return vm
