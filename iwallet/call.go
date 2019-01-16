1d626c5921db1c6db3f8778f844b55bd2859a22d
Rey Li <lilei_cool1@163.com>
2019-01-16 14:29:38 +0800

    fix  size of actions

--- iwallet/call.go
+++ iwallet/call.go
@@ -41,7 +41,7 @@ var callCmd = &cobra.Command{
 		if argc%3 != 0 {
 			return fmt.Errorf(`number of args should be a multiplier of 3`)
 		}
-		var actions = make([]*rpcpb.Action, argc/3)
+		var actions = make([]*rpcpb.Action, 0)
 		for i := 0; i < len(args); i += 3 {
 			act := NewAction(args[i], args[i+1], args[i+2]) //check sth here
 			actions = append(actions, act)
