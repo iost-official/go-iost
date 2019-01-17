e8e90f525ae8592926bc97ef53cb94bee8203c0d
Meng Laijun <mlj1991@gmail.com>
2019-01-17 15:40:27 +0800

    Update witness test script

--- script/test/witness_test.sh
+++ script/test/witness_test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-readonly RPC_URL="http://127.0.0.1:30001"
+readonly HTTP_URL="http://127.0.0.1:30001"
+readonly GRPC_URL="127.0.0.1:30002"
 readonly VOTE_ACCOUNT="admin"
 readonly VOTE_ACCOUNT_SECKEY="2yquS3ySrGWPEKywCPzX4RTJugqRh7kJSo5aehsLYPEWkUxBWA39oMrZ7ZxuM4fgyXYs2cPwh5n8aNNpH5x2VyK1"
-readonly REPLACE_WITNESS="producer01"
+readonly REPLACE_WITNESS="producer000"
 
-readonly WITNESS_NUM=33
+readonly WITNESS_NUM=0
 
 readonly WITNESS_NAME=(
-
 )
 
 function err
@@ -18,60 +18,69 @@ function err
 
 function init_state
 {
-    iwallet account --import ${VOTE_ACCOUNT} ${VOTE_ACCOUNT_SECKEY}
-
-    iwallet --account ${VOTE_ACCOUNT} call "vote_producer.iost" "unvote" '["'${VOTE_ACCOUNT}'", "'${REPLACE_WITNESS}'", "3000000"]'
+    iwallet -s ${GRPC_URL} account --import ${VOTE_ACCOUNT} ${VOTE_ACCOUNT_SECKEY}
+    iwallet -s ${GRPC_URL} --account ${VOTE_ACCOUNT} call "vote_producer.iost" "unvote" '["'${VOTE_ACCOUNT}'", "'${REPLACE_WITNESS}'", "3000000"]'
 }
 
 function get_now_height
 {
-    return iwallet state | grep '"headBlock"' | tr -cd "[0-9]"
+    echo $(iwallet -s ${GRPC_URL} state | grep '"headBlock"' | tr -cd "[0-9]")
 }
 
 function get_witness_by_height
 {
-    return curl ${RPC_URL}/getBlockByNumber/$1/true | grep -Eo '"witness":".*?"' | cut -d: -f2 | tr -cd "[0-9,a-z,A-Z]"
+    echo $(curl -s ${HTTP_URL}/getBlockByNumber/$1/true | grep -Eo '"witness":".*?"' | cut -d: -f2 | tr -cd "[0-9,a-z,A-Z]")
 }
 
 function get_pubkey_by_witness
 {
-    return curl -X POST ${RPC_URL}/getContractStorage -d '{"id":"vote_producer.iost","key":"producerTable","field":"'$1'","by_longest_chain":true}' | grep -oE 'pubkey\\":\\".*?"' | cut -d: -f2 | tr -cd "[0-9,a-z,A-Z]"
+    echo $(curl -s -X POST ${HTTP_URL}/getContractStorage -d '{"id":"vote_producer.iost","key":"producerTable","field":"'$1'","by_longest_chain":true}' | grep -oE 'pubkey\\":\\".*?"' | cut -d: -f2 | tr -cd "[0-9,a-z,A-Z]")
 }
 
 function gen_block_success
 {
-    start = get_now_height
-    end = start + 200
+    local i
+    start=$(get_now_height)
+    end=$((${start} + 2200))
 
     for (( i = start; i < end; i++ ))
     do
-        if [[ $(get_witness_by_height $i) == $1 ]]
+        witness=$(get_witness_by_height $i)
+        echo "check block from $start to $end, wait for witness $1, now height is $i, witness is $witness"
+        if [[ $witness == $1 ]]
         then
-            return true
-        if
+            return 0
+        fi
     done
-    return false
+    return 1
 }
 
 function check_witness
 {
-    for i in {0..WITNESS_NUM-1}
+    local i
+    for (( i = 0; i < ${WITNESS_NUM}; i++ ))
     do
-        iwallet --account ${VOTE_ACCOUNT} call "vote_producer.iost" "vote" '["'${VOTE_ACCOUNT}'", "'${WITNESS_NAME[i]}'", "300000000"]'
+        iwallet -s ${GRPC_URL} --account ${VOTE_ACCOUNT} call "vote_producer.iost" "vote" '["'${VOTE_ACCOUNT}'", "'${WITNESS_NAME[i]}'", "3000000"]'
 
-        witness_pubkey = get_pubkey_by_witness ${WITNESS_NAME[i]}
+        witness_pubkey=$(get_pubkey_by_witness ${WITNESS_NAME[i]})
+        echo "${WITNESS_NAME[i]}'s pubkey is $witness_pubkey"
 
-        if [[ gen_block_success $witness_pubkey ]]
+        if
+        gen_block_success $witness_pubkey
         then
-            echo "${WITNESS_NAME[i]} gen block successful!"
+            err "${WITNESS_NAME[i]} gen block successful!"
         else
             err "${WITNESS_NAME[i]} gen block failed!"
         fi
-        iwallet --account ${VOTE_ACCOUNT} call "vote_producer.iost" "unvote" '["'${VOTE_ACCOUNT}'", "'${WITNESS_NAME[i]}'", "300000000"]'
+        iwallet -s ${GRPC_URL} --account ${VOTE_ACCOUNT} call "vote_producer.iost" "unvote" '["'${VOTE_ACCOUNT}'", "'${WITNESS_NAME[i]}'", "3000000"]'
     done
 }
 
 function reset_state
 {
-    iwallet --account ${VOTE_ACCOUNT} call "vote_producer.iost" "vote" '["'${VOTE_ACCOUNT}'", "'${REPLACE_WITNESS}'", "3000000"]'
+    iwallet -s ${GRPC_URL} --account ${VOTE_ACCOUNT} call "vote_producer.iost" "vote" '["'${VOTE_ACCOUNT}'", "'${REPLACE_WITNESS}'", "3000000"]'
 }
+
+init_state
+check_witness
+reset_state
--- script/test/witness_vote.sh
+++ script/test/witness_vote.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+readonly GRPC_URL="127.0.0.1:30002"
+readonly VOTER_ACCOUNT="admin"
+readonly VOTER_ACCOUNT_SECKEY="2yquS3ySrGWPEKywCPzX4RTJugqRh7kJSo5aehsLYPEWkUxBWA39oMrZ7ZxuM4fgyXYs2cPwh5n8aNNpH5x2VyK1"
 
-readonly VOTE_ACCOUNT="admin"
-readonly VOTE_ACCOUNT_SECKEY="2yquS3ySrGWPEKywCPzX4RTJugqRh7kJSo5aehsLYPEWkUxBWA39oMrZ7ZxuM4fgyXYs2cPwh5n8aNNpH5x2VyK1"
-
-readonly WITNESS_NUM=17
+readonly WITNESS_NUM=1
 
 readonly WITNESS_NAME=(
-
+producer000
 )
 
-iwallet account --import ${VOTE_ACCOUNT} ${VOTE_ACCOUNT_SECKEY}
-for i in {0..IOST_WITNESS_NUM-1}
+iwallet account --import ${VOTER_ACCOUNT} ${VOTER_ACCOUNT_SECKEY}
+for (( i = 0; i < ${WITNESS_NUM}; i++ ))
 do
-    iwallet --account ${VOTE_ACCOUNT} call "vote_producer.iost" "vote" '["'${VOTE_ACCOUNT}'", "'${IOST_WITNESS_NAME[i]}'", "3000000"]'
+    iwallet -s ${GRPC_URL} --account ${VOTER_ACCOUNT} call "vote_producer.iost" "vote" '["'${VOTER_ACCOUNT}'", "'${WITNESS_NAME[i]}'", "3000000"]'
 done
