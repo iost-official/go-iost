8c7ebe05b737d786114e7e385ae34905818be08a
liuziran <1213092802@qq.com>
2019-01-19 17:49:47 +0800

    fix indent

--- config/genesis/contract/exchange.js
+++ config/genesis/contract/exchange.js
@@ -50,7 +50,7 @@ class Exchange {
                 // create account and then transfer to account
                 let args = memo.split(":").slice(1);
                 if (args.length !== 3) {
-                  throw new Error("memo of transferring to a new account should be of format create:name:ownerKey:activeKey");
+                    throw new Error("memo of transferring to a new account should be of format create:name:ownerKey:activeKey");
                 }
                 blockchain.call("auth.iost", "signUp", args);
                 let rets = blockchain.call("ram.iost", "buy", [from, args[0], initialRAM]);
