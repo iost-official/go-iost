d096bd700e95d2e5690ae2dae68bc5617ad284c5
Rey Li <lilei_cool1@163.com>
2019-01-23 18:34:57 +0800

    del slotUsed

--- consensus/pob/pob.go
+++ consensus/pob/pob.go
@@ -315,19 +315,24 @@ func (p *PoB) blockLoop() {
 func (p *PoB) scheduleLoop() {
 	defer p.wg.Done()
 
-	tGenBlock := time.NewTicker(20 * time.Microsecond)
+	tGenBlock := time.NewTicker(20 * time.Millisecond)
 	tMetricsMode := time.NewTicker(3 * time.Second)
 	defer tGenBlock.Stop()
 	defer tMetricsMode.Stop()
 
+	var slotFlag int64
 	for {
 		select {
 		case <-tGenBlock.C:
+			// Don't delete,avoid time error
+			time.Sleep(1 * time.Millisecond)
+
 			t := time.Now()
 			_, head := p.txPool.PendingTx()
 			witnessList := head.Active()
 			pubkey := p.account.ReadablePubkey()
-			if witnessOfNanoSec(t.UnixNano(), witnessList) == pubkey && !staticProperty.SlotUsed[slotOfSec(t.Unix())] && p.baseVariable.Mode() == global.ModeNormal {
+			if witnessOfNanoSec(t.UnixNano(), witnessList) == pubkey && slotFlag != slotOfSec(t.Unix()) && p.baseVariable.Mode() == global.ModeNormal {
+				slotFlag = slotOfSec(t.Unix())
 				p.quitGenerateMode = make(chan struct{})
 				staticProperty.SlotUsed[t.Unix()] = true // never delete
 				generateBlockTicker := time.NewTicker(subSlotTime)
