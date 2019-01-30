8fc9fa8f41191415bac2f33ef048ee06f6a1f462
hudongwen <ianhuu.x@gmail.com>
2019-01-30 20:21:29 +0800

    fix JSON.parse

--- vm/v8vm/v8/libjs/environment.js
+++ vm/v8vm/v8/libjs/environment.js
@@ -448,10 +448,10 @@
     // JSON
     const JSONparse = JSON.parse;
     JSON.parse = function () {
-        if (typeof arguments[0] === 'string' || arguments[0] instanceof String) {
-            _IOSTInstruction_counter.incr(10 + arguments[0].length);
-        } else {
+        if (arguments[0] === undefined || arguments[0] === null) {
             _IOSTInstruction_counter.incr(10);
+        } else {
+            _IOSTInstruction_counter.incr(10 + arguments[0].toString().length);
         }
         return JSONparse.call(this, ...arguments);
     };
