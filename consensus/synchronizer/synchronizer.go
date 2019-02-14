af62190ead76250b564498274b85a671e0654c85
liangjn16 <liangjn16@mails.tsinghua.edu.cn>
2019-02-14 15:10:36 +0800

    update log

--- consensus/synchronizer/synchronizer.go
+++ consensus/synchronizer/synchronizer.go
@@ -515,7 +515,7 @@ func (sy *SyncImpl) reqSyncBlock(hash string, p interface{}, peerID interface{})
 		ilog.Errorf("Assert p to int64 failed. p=%v", p)
 		return false, false
 	}
-	ilog.Debugf("callback try sync block, num:%v", bn)
+	ilog.Debugf("callback try sync block, num:%v, hash:%v", bn, hash)
 	if bn <= sy.blockCache.LinkedRoot().Head.Number {
 		ilog.Debugf("callback block confirmed, num:%v", bn)
 		return false, true
