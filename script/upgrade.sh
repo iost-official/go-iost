7b2877b0bf2eb80d2dac4a10072b45fc0dfdf335
Jack Ji <yuanyi@iost.io>
2019-02-16 18:30:56 +0800

    Add iserver version info after upgrade

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -11,6 +11,8 @@ set -ue
 PREFIX=${PREFIX:="/data/iserver"}
 
 CURL="curl -fsSL"
+PYTHON=${PYTHON:=python}
+
 USR_LOCAL_BIN=${USR_LOCAL_BIN:=/usr/local/bin}
 export PATH=$PATH:$USR_LOCAL_BIN
 
@@ -20,7 +22,10 @@ export PATH=$PATH:$USR_LOCAL_BIN
 
 print_bye() {
     {
-        echo Upgrade done.
+        echo
+        echo Upgrade done. iServer info:
+        set +e; docker inspect iserver | $PYTHON -c "import json,sys;d=json.load(sys.stdin)[0];print(d['Config']['Image']);print(d['Image'])"; set -e
+        echo
         echo Happy hacking !
         echo
     }>&2
