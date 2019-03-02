181c9af6072b9f6364df5b7e815ba8299b7e6d7d
Meng Laijun <mlj1991@gmail.com>
2019-03-03 00:29:37 +0800

    Add request handler for sync module

---
+++ consensus/sync/request_handler.go
@@ -0,0 +1,73 @@
+package sync
+
+import (
+	"github.com/iost-official/go-iost/core/block"
+	"github.com/iost-official/go-iost/core/blockcache"
+	"github.com/iost-official/go-iost/ilog"
+	"github.com/iost-official/go-iost/p2p"
+	"sync"
+)
+
+type requestHandler struct {
+	p      p2p.Service
+	bCache blockcache.BlockCache
+	bChain *block.BlockChain
+
+	requestCh chan p2p.IncomingMessage
+
+	quitCh chan struct{}
+	done   *sync.WaitGroup
+}
+
+func newRequestHandler(p p2p.Service, bCache blockcache.BlockCache, bCahin *block.BlockChain) *requestHandler {
+	rHandler := &requestHandler{
+		p:      p,
+		bCache: bCache,
+		bChain: bCahin,
+
+		requestCh: p.Register("sync request", p2p.SyncBlockHashRequest, p2p.SyncBlockRequest),
+
+		quitCh: make(chan struct{}),
+		done:   new(sync.WaitGroup),
+	}
+
+	rHandler.done.Add(1)
+	go rHandler.controller()
+
+	return rHandler
+}
+
+func (r *requestHandler) controller() {
+	for {
+		select {
+		case request := <-r.requestCh:
+			switch request.Type() {
+			case p2p.SyncBlockHashRequest:
+				r.handleBlockHashRequest(&request)
+			case p2p.SyncBlockRequest:
+				r.handleBlockRequest(&request)
+			default:
+				ilog.Warnf("Unexcept request type: %v", request.Type())
+			}
+		default:
+		}
+		select {
+		case <-r.quitCh:
+			r.done.Done()
+			return
+		default:
+		}
+	}
+}
+
+func (r *requestHandler) close() {
+	close(r.quitCh)
+	r.done.Wait()
+	ilog.Infof("Stoped sync request handler.")
+}
+
+func (r *requestHandler) handleBlockHashRequest(request *p2p.IncomingMessage) {
+}
+
+func (r *requestHandler) handleBlockRequest(request *p2p.IncomingMessage) {
+}
