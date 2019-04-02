3a10f0685b81d27f5fd847f7d0639e7ea82fb243
Meng Laijun <mlj1991@gmail.com>
2019-04-02 18:26:12 +0800

    Remove topBlock from generateBlock.

--- consensus/pob/algorithm.go
+++ consensus/pob/algorithm.go
@@ -24,26 +24,25 @@ func generateBlock(
 
 	ilog.Debug("generate Block start")
 	st := time.Now()
-	topBlock := head.Block
 	blk := &block.Block{
 		Head: &block.BlockHead{
 			Version:    0,
-			ParentHash: topBlock.HeadHash(),
+			ParentHash: head.HeadHash(),
 			Info:       make([]byte, 0),
-			Number:     topBlock.Head.Number + 1,
+			Number:     head.Head.Number + 1,
 			Witness:    acc.ReadablePubkey(),
 			Time:       time.Now().UnixNano(),
 		},
 		Txs:      []*tx.Tx{},
 		Receipts: []*tx.TxReceipt{},
 	}
-	db.Checkout(string(topBlock.HeadHash()))
+	db.Checkout(string(head.HeadHash()))
 
 	// call vote
 	v := verifier.Verifier{}
 	t1 := time.Now()
 	// TODO: stateDb and block head is consisdent, pTx may be inconsisdent.
-	dropList, _, err := v.Gen(blk, topBlock, &head.WitnessList, db, pTx, &verifier.Config{
+	dropList, _, err := v.Gen(blk, head.Block, &head.WitnessList, db, pTx, &verifier.Config{
 		Mode:        0,
 		Timeout:     limitTime - time.Now().Sub(st),
 		TxTimeLimit: common.MaxTxTimeLimit,
