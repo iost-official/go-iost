a1fae2c410976716379de2bf615d39e9a8010be1
Meng Laijun <mlj1991@gmail.com>
2019-04-03 19:23:02 +0800

    Add comment for TimeOfBlock.

--- common/type.go
+++ common/type.go
@@ -99,6 +99,7 @@ func NextSlot() int64 {
 	return time.Now().UnixNano()/int64(SlotInterval) + 1
 }
 
+// TimeOfBlock will return the block time for specific slots and num.
 func TimeOfBlock(slot int64, num int64) time.Time {
 	unixNano := slot*int64(SlotInterval) + num*int64(BlockInterval)
 	return time.Unix(unixNano/int64(time.Second), unixNano%int64(time.Second))
