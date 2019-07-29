78c677aa2ae2bd680be7b7f2b72fd45c7d0eebc2
Rey Li <lilei_cool1@163.com>
2019-07-29 13:14:44 +0800

    code optimization

--- common/bytes.go
+++ common/bytes.go
@@ -2,39 +2,8 @@ package common
 
 import (
 	"encoding/binary"
-	"encoding/hex"
 	"math"
 )
-
-// FromHex string to bytes
-func FromHex(s string) []byte {
-	if len(s) > 1 {
-		if s[0:2] == "0x" || s[0:2] == "0X" {
-			s = s[2:]
-		}
-	}
-	if len(s)%2 == 1 {
-		s = "0" + s
-	}
-	return Hex2Bytes(s)
-}
-
-// Hex2Bytes hex string to bytes
-func Hex2Bytes(str string) []byte {
-	h, _ := hex.DecodeString(str)
-	return h
-}
-
-// CopyBytes ...
-func CopyBytes(b []byte) (copiedBytes []byte) {
-	if b == nil {
-		return nil
-	}
-	copiedBytes = make([]byte, len(b))
-	copy(copiedBytes, b)
-
-	return
-}
 
 // Int64ToBytes converts int64 to byte slice.
 func Int64ToBytes(n int64) []byte {
