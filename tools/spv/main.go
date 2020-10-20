626fdc5b2d2f049e756fa948afb24c24a46e0802
张卓 <mycinbrin@gmail.com>
2020-10-20 17:26:57 +0800

    add spv example

---
+++ tools/spv/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/iost-official/go-iost/common"
+	"github.com/iost-official/go-iost/core/block"
+	rpcpb "github.com/iost-official/go-iost/rpc/pb"
+	"google.golang.org/grpc"
+)
+
+func testVerifier() error {
+	verifier := Verifier{}
+	server := "54.180.196.80:30002"
+	rpcConn, err := grpc.Dial(server, grpc.WithInsecure())
+	if err != nil {
+		return err
+	}
+	client := rpcpb.NewApiServiceClient(rpcConn)
+	startBlock := (int64)(102504000)
+	blk, err := client.GetRawBlockByNumber(context.Background(), &rpcpb.GetBlockByNumberRequest{Number: startBlock, Complete: true})
+	if err != nil {
+		return err
+	}
+	b := &block.Block{}
+	b.FromPb(blk.Block)
+	err = verifier.init(b)
+	if err != nil {
+		return err
+	}
+	// try to verify some blocks within next 1 hour
+	for delta := 60; delta <= 7200; delta += 60 {
+		blockNum := startBlock + int64(delta)
+		blk, err := client.GetRawBlockByNumber(context.Background(), &rpcpb.GetBlockByNumberRequest{Number: blockNum, Complete: true})
+		if err != nil {
+			return err
+		}
+		b := &block.Block{}
+		b.FromPb(blk.Block)
+		blockList, err := client.GetBlockHeaderByRange(context.Background(), &rpcpb.GetBlockHeaderByRangeRequest{
+			Start: blockNum + 1,
+			End:   blockNum + 1 + 108,
+		})
+		if err != nil {
+			return err
+		}
+		blkList := make([]*block.Block, 0, 108)
+		for _, item := range blockList.BlockList {
+			b := &block.Block{}
+			b.FromPb(item)
+			blkList = append(blkList, b)
+		}
+		if b.Head.Number%common.VoteInterval == 0 {
+			err := verifier.updateEpoch(b, blkList)
+			if err != nil {
+				return err
+			}
+		} else {
+			err := verifier.checkBlock(b, blkList)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	fmt.Println("test done")
+	return nil
+}
+
+func main() {
+	err := testVerifier()
+	if err != nil {
+		panic(err)
+	}
+}
